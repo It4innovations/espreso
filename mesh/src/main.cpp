f904a6527ecaf60de5a7cd47c0c4e40ffffb6d0b
Ondrej Meca <ondrej.meca@vsb.cz>
2015-05-05 10:23:08 +0200

    ENH: change main.cpp

--- mesh/src/main.cpp
+++ mesh/src/main.cpp
@@ -5,12 +5,17 @@ int main(int argc, char** argv)
 	int partsCount = 4;
 	int fixPointsCount = 8;
 
-	Coordinates coords("matrices/HEX/5/coord");
+	Coordinates coords("matrices/HEX/15/coord");
 	Mesh mesh(coords);
-	mesh = Mesh("matrices/HEX/5/elem", coords, partsCount, fixPointsCount);
-	Boundaries boundaries(mesh, coords);
-	Faces faces(mesh, coords);
-	Corners corners(faces.getFaces(), coords);
+	mesh = Mesh("matrices/HEX/15/elem", coords, partsCount, fixPointsCount);
 
-	mesh.saveVTK();
+	int dimension = mesh.getPartNodesCount(0) * Point::size();
+
+	SparseCSRMatrix K(dimension, dimension);
+	SparseCSRMatrix M(dimension, dimension);
+	std::vector<double> f(dimension);
+
+	mesh.assemble_matrix(K, M, f, 0);
+
+	//mesh.saveVTK();
 }
