a28ec81c652162e0a7fd07e43d1201ab7b10c041
mar440 <mar440@.vsb.cz>
2015-06-12 10:19:33 +0200

    prisma15: partial changes added

---
+++ mesh/src/elements/3D/prisma15.cpp
@@ -0,0 +1,245 @@
+#include "prisma15.h"
+
+using namespace mesh;
+
+std::vector< std::vector< double> > Prisma15_rst()
+{
+	std::vector< std::vector<double> > rst(3, std::vector<double>(Prisma15GPCount));
+
+	switch (Prisma15GPCount) {
+	case 9: {
+		double v1 = 1.0 / 6.0;
+		double v2 = 4.0 / 6.0;
+		double v3 = sqrt(3.0 / 5.0);
+		double v4 = 0.0;
+		double r[] = {  v1,  v2,  v1,  v1,  v2,  v1,  v1,  v2,  v1 };
+		double s[] = {  v1,  v1,  v2,  v1,  v1,  v2,  v1,  v1,  v2 };
+		double t[] = { -v3, -v3, -v3,  v4,  v4,  v4,  v3,  v3,  v3 };
+		rst[0].assign(r, r + 9);
+		rst[1].assign(s, s + 9);
+		rst[2].assign(t, t + 9);
+		return rst;
+	}
+	default:
+		std::cerr << "Unknown number of Prisma15 GP count\n";
+		exit(EXIT_FAILURE);
+	}
+}
+
+
+std::vector< std::vector< double> > _prisma15_rst = Prisma15_rst();
+
+std::vector<DenseMatrix> Prisma15_dN() {
+	std::vector<DenseMatrix> dN(
+		Prisma15GPCount,
+		DenseMatrix(Point::size(), Prisma15NodesCount)
+	);
+
+	for (unsigned int i = 0; i < Prisma15GPCount; i++) {
+		///dN contains [dNr, dNs, dNt]
+		DenseMatrix &m = dN[i];
+
+		double r = _prisma15_rst[0][i];
+		double s = _prisma15_rst[1][i];
+		double t = _prisma15_rst[2][i];
+
+		// dNr - derivation of basis function
+		 m(0, 0) = - (t - 1.0)*(r + s - 1.0) - ((t - 1.0)*(2.0*r + 2.0*s + t))/2.0;
+		 m(0, 1) =  ((t - 1.0)*(t - 2.0*r + 2.0))/2.0 - r*(t - 1.0);
+		 m(0, 2) =    0.0;
+		 m(0, 3) =  ((t + 1.0)*(2.0*r + 2.0*s - t))/2.0 + (t + 1.0)*(r + s - 1.0);
+		 m(0, 4) =    r*(t + 1.0) + ((t + 1.0)*(2.0*r + t - 2.0))/2.0;
+		 m(0, 5) =   0.0;
+		 m(0, 6) =   2.0*(t - 1.0)*(r + s - 1.0) + 2.0*r*(t - 1.0);
+		 m(0, 7) =  (-2.0)*s*(t - 1.0);
+		 m(0, 8) =   2.0*s*(t - 1.0);
+		 m(0, 9) =  -2.0*(t + 1.0)*(r + s - 1.0) - 2.0*r*(t + 1.0);
+		 m(0,10) =   2.0*s*(t + 1.0);
+		 m(0,11) =  -2.0*s*(t + 1.0);
+		 m(0,12) =   pow(t,2.0) - 1.0;
+		 m(0,13) =   1.0 - pow(t,2.0);
+		 m(0,14) =   0.0;
+
+
+		// dNs - derivation of basis function
+		 m(1, 0) =  - (t - 1.0)*(r + s - 1.0) - ((t - 1.0)*(2.0*r + 2.0*s + t))/2.0;
+		 m(1, 1) =  0.0;
+		 m(1, 2) =  ((t - 1.0)*(t - 2.0*s + 2.0))/2.0 - s*(t - 1.0);
+		 m(1, 3) =  ((t + 1.0)*(2.0*r + 2.0*s - t))/2.0 + (t + 1.0)*(r + s - 1.0);
+		 m(1, 4) =  0.0;
+		 m(1, 5) =  s*(t + 1.0) + ((t + 1.0)*(2.0*s + t - 2.0))/2.0;
+		 m(1, 6) =  2.0*r*(t - 1.0);
+		 m(1, 7) =  (-2.0)*r*(t - 1.0);
+		 m(1, 8) =  2.0*(t - 1.0)*(r + s - 1.0) + 2.0*s*(t - 1.0);
+		 m(1, 9) =  (-2.0)*r*(t + 1.0);
+		 m(1,10) =  2.0*r*(t + 1.0);
+		 m(1,11) =  - 2.0*(t + 1.0)*(r + s - 1.0) - 2.0*s*(t + 1.0);
+		 m(1,12) =  pow(t,2.0) - 1.0;
+		 m(1,13) =  0.0;
+		 m(1,14) =  1.0 - pow(t,2.0);
+
+		// dNt - derivation of basis function
+		 m(2, 0) = - ((r + s - 1.0)*(2.0*r + 2.0*s + t))/2.0 - ((t - 1.0)*(r + s - 1.0))/2.0;
+		 m(2, 1) =  (r*(t - 2.0*r + 2.0))/2.0 + (r*(t - 1.0))/2.0;
+		 m(2, 2) =  (s*(t - 2.0*s + 2.0))/2.0 + (s*(t - 1.0))/2.0;
+		 m(2, 3) = ((r + s - 1.0)*(2.0*r + 2.0*s - t))/2.0 - ((t + 1.0)*(r + s - 1.0))/2.0;
+		 m(2, 4) = (r*(2.0*r + t - 2.0))/2.0 + (r*(t + 1.0))/2.0;
+		 m(2, 5) = (s*(2.0*s + t - 2.0))/2.0 + (s*(t + 1.0))/2.0;
+		 m(2, 6) =  2.0*r*(r + s - 1.0);
+		 m(2, 7) =  (-2.0)*r*s;
+		 m(2, 8) =  2.0*s*(r + s - 1.0);
+		 m(2, 9) =  (-2.0)*r*(r + s - 1.0);
+		 m(2,10) =  2.0*r*s;
+		 m(2,11) = (-2.0)*s*(r + s - 1.0);
+		 m(2,12) = 2.0*t*(r + s - 1.0);
+		 m(2,13) = (-2.0)*r*t;
+		 m(2,14) = (-2.0)*s*t;
+	}
+
+	return dN;
+}
+
+std::vector<DenseMatrix> Prisma15_N() {
+	std::vector<DenseMatrix> N(
+		Prisma15GPCount,
+		DenseMatrix(1, Prisma15NodesCount)
+	);
+
+	for (unsigned int i = 0; i < Prisma15GPCount; i++) {
+		DenseMatrix &m = N[i];
+
+		double r = _prisma15_rst[0][i];
+		double s = _prisma15_rst[1][i];
+		double t = _prisma15_rst[2][i];
+
+		// basis function
+    m(0, 0) = -(1.0-r-s)*(1.0-t)*(2.0*r+2.0*s+t)/2.0;
+		m(0, 1) =  r*(1.0-t)*(2.0*r-t-2.0)/2.0;
+		m(0, 2) =  s*(1.0-t)*(2.0*s-t-2.0)/2.0;
+		m(0, 3) =  -(1.0-r-s)*(1.0+t)*(2.0*r+2.0*s-t)/2.0;
+		m(0, 4) =  r*(t+1.0)*(2.0*r+t-2.0)/2.0;
+		m(0, 5) =  s*(t+1.0)*(2.0*s+t-2.0)/2.0;
+		m(0, 6) =  2.0*r*(1.0-r-s)*(1.0-t);
+		m(0, 7) =  2.0*r*s*(1.0-t);
+		m(0, 8) =  2.0*s*(1.0-r-s)*(1.0-t);
+		m(0, 9) =  2.0*r*(1.0-r-s)*(1.0+t);
+		m(0,10) = 2.0*r*s*(1.0+t);
+		m(0,11) = 2.0*s*(1.0-r-s)*(1.0+t);
+		m(0,12) = (1.0-r-s)*(1.0-pow(t,2.0));
+		m(0,13) = r*(1.0-pow(t,2.0));
+		m(0,14) = s*(1.0-pow(t,2.0));
+	}
+
+	return N;
+}
+
+std::vector<double> Prisma15_weight()
+{
+	switch (Prisma15GPCount) {
+	case 9: {
+		double v1 = 5.0 / 54.0;
+		double v2 = 8.0 / 54.0;
+		double w[9] = { v1, v1, v1, v2, v2, v2, v1, v1, v1 };
+		return std::vector<double> (w, w + 9);
+	}
+	default:
+		std::cerr << "Unknown number of Tatrahedron10 GP count\n";
+		exit(EXIT_FAILURE);
+	}
+}
+
+std::vector<DenseMatrix> Prisma15::_dN = Prisma15_dN();
+std::vector<DenseMatrix> Prisma15::_N = Prisma15_N();
+std::vector<double> Prisma15::_weighFactor = Prisma15_weight();
+
+bool Prisma15::match(eslocal *indices, eslocal n) {
+
+#if ESPRESO_POINT_DIMENSION == 2
+	// Hexahedron8 is 3D element
+	return false;
+#endif
+
+	if (n != 20) {
+		return false;
+	}
+
+	if (!Element::match(indices, 2, 3)) {
+		return false;
+	}
+	if (!Element::match(indices, 6, 7)) {
+		return false;
+	}
+
+	eslocal various[6] = { 0, 1, 2, 4, 5, 6 };
+	for (eslocal i = 0; i < 5; i++) {
+		for (eslocal j = i + 1; j < 6; j++) {
+			if (Element::match(indices, various[i], various[j])) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+std::vector<eslocal> Prisma15::getNeighbours(size_t nodeIndex) const
+{
+	std::vector<eslocal> result(3);
+
+	if (nodeIndex > 2) {
+		result[0] = _indices[nodeIndex - 3];
+		result[1] = _indices[(nodeIndex + 1) % 3 + 3];
+		result[2] = _indices[(nodeIndex + 2) % 3 + 3];
+	} else {
+		result[0] = _indices[nodeIndex + 3];
+		result[1] = _indices[(nodeIndex + 1) % 3];
+		result[2] = _indices[(nodeIndex + 2) % 3];
+	}
+
+	return result;
+}
+
+std::vector<eslocal> Prisma15::getFace(size_t face) const
+{
+	// bottom
+	if (face == 3) {
+		std::vector<eslocal> result(3);
+		result[0] = _indices[0];
+		result[1] = _indices[1];
+		result[2] = _indices[2];
+		return result;
+	}
+
+	// top
+	if (face == 4) {
+		std::vector<eslocal> result(3);
+		result[0] = _indices[3];
+		result[1] = _indices[4];
+		result[2] = _indices[5];
+		return result;
+	}
+
+	//sides
+	std::vector<eslocal> result(4);
+	result[0] = _indices[ face              ];
+	result[1] = _indices[(face + 1) % 3     ];
+	result[2] = _indices[(face + 1) % 3 + 3 ];
+	result[3] = _indices[ face + 3          ];
+	return result;
+}
+
+Prisma15::Prisma15(eslocal *indices)
+{
+	_indices[0] = indices[0];
+	_indices[1] = indices[1];
+	_indices[2] = indices[2];
+	_indices[3] = indices[4];
+	_indices[4] = indices[5];
+	_indices[5] = indices[6];
+}
+
+
+
+
+
+
---
+++ mesh/src/elements/3D/prisma15.h
@@ -0,0 +1,91 @@
+
+#ifndef PRISMA15_H_
+#define PRISMA15_H_
+
+#include "../element.h"
+#include "../1D/line.h"
+
+#define Prisma15NodesCount 15
+#define Prisma15FacesCount 5
+#define Prisma15GPCount 9
+#define Prisma15VTKCode 13
+
+namespace mesh {
+
+class Prisma15: public Element
+{
+
+public:
+	static bool match(eslocal *indices, eslocal n);
+
+	Prisma15(eslocal *indices);
+
+	Element* copy() const
+	{
+		return new Prisma15(*this);
+	}
+
+	eslocal vtkCode() const
+	{
+		return Prisma15VTKCode;
+	}
+
+	const eslocal* indices() const
+	{
+		return _indices;
+	}
+
+	size_t size() const
+	{
+		return Prisma15NodesCount;
+	}
+
+	size_t gpSize() const
+	{
+		return Prisma15GPCount;
+	}
+
+	size_t faces() const
+	{
+		return Prisma15FacesCount;
+	}
+
+	const std::vector<DenseMatrix>& dN() const
+	{
+		return Prisma15::_dN;
+	}
+
+	const std::vector<DenseMatrix>&  N() const
+	{
+		return Prisma15::_N;
+	}
+
+	const std::vector<double>& weighFactor() const
+	{
+		return Prisma15::_weighFactor;
+	}
+
+	std::vector<eslocal> getNeighbours(size_t nodeIndex) const;
+	std::vector<eslocal> getFace(size_t face) const;
+
+protected:
+
+	eslocal* indices()
+	{
+		return _indices;
+	}
+
+private:
+	inline void setFaceNodes(eslocal nodes[], eslocal face) const;
+
+	eslocal _indices[Prisma15NodesCount];
+
+	static std::vector<DenseMatrix> _dN;
+	static std::vector<DenseMatrix> _N;
+	static std::vector<double> _weighFactor;
+};
+
+}
+
+
+#endif /* PRISMA6_H_ */
