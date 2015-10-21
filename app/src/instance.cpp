92c9f2a6dca203ad10b83fd1b598ea202826606d
Ondrej Meca <ondrej.meca@vsb.cz>
2015-10-21 09:23:42 +0200

    FIX: sphere respects outer radius

--- app/src/instance.cpp
+++ app/src/instance.cpp
@@ -5,6 +5,7 @@ Instance::Instance(int argc, char** argv, int rank, int size)
 	: _mesh(rank, size), _rank(rank), _size(size)
 {
 	_mesh.load(mesh::MESH_GENERATOR, argc, argv);
+	//_mesh.store(mesh::VTK, "sphere", 0.9, 0.9);
 }
 
 
