efa87312d72c567103da858a4b1cc4cb55a497eb
Ondrej Meca <ondrej.meca@vsb.cz>
2015-11-10 08:22:54 +0100

    FIX: add forgotten files

---
+++ mesh/esmesh.h
@@ -0,0 +1,10 @@
+
+#ifndef MESH_ESMESH_H_
+#define MESH_ESMESH_H_
+
+#include "structures/coordinates.h"
+#include "structures/boundaries.h"
+#include "structures/mesh.h"
+
+
+#endif /* MESH_ESMESH_H_ */
