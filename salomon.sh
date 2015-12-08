d83f9050cf16be48b05917b52993cb40a8824e54
lriha <lubomir.riha@vsb.cz>
2015-12-08 15:35:08 +0100

    fixes in salomon.sh

--- salomon.sh
+++ salomon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-INTEL=/apps/all/icc/2015.3.187
-. $INTEL/tbb/bin/tbbvars.sh intel64
+#INTEL=/apps/all/icc/2015.3.187
+#. $INTEL/tbb/bin/tbbvars.sh intel64
 
 if [ "$1" = "runpbs" ]; then
 
@@ -54,19 +54,20 @@ fi
 module load impi/5.0.3.048-iccifort-2015.3.187
 module load icc/2015.3.187
 module load imkl/11.2.3.187-iimpi-7.3.5
+
+#module load impi/5.1.2.150-iccifort-2016.1.150-GCC-4.9.3
+#module load icc/2016.1.150-GCC-4.9.3 
+#module load imkl/11.3.1.150-iimpi-2016.00-GCC-4.9.3
+module load tbb/4.4.2.152
 #module load DDT/5.0.1
 
-
-#export INTEL=/apps/all/icc/2015.3.187
-#export IMPI=/apps/all/impi/5.0.3.048-iccifort-2015.3.187
-
-#. $INTEL/bin/compilervars.sh intel64
-#. $IMPI/bin64/mpivars.sh
-
-#module load CMake/3.0.0-intel-2015b 
-#module load tbb/4.3.5.187
-#. $INTEL/tbb/bin/tbbvars.sh intel64
-
+export MKL_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+export SOLVER_NUM_THREADS=16
+export PAR_NUM_THREADS=16
+export CILK_NWORKERS=16
+export PARDISOLICMESSAGE=1
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./libs
 export LC_CTYPE=""
 
 if [ "$#" -eq 0 ]; then
@@ -124,37 +125,16 @@ el_type=(   0     1      2       3       4       5        6         7)
 
 if [ "$1" = "run" ]; then
 
-  export PARDISOLICMESSAGE=1 
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./libs
-
-  export OMP_NUM_THREADS=1
-  export MKL_NUM_THREADS=1
-  export SOLVER_NUM_THREADS=24
-  export PAR_NUM_THREADS=24
-
-#  export CILK_NWORKERS=1 # 24
-#  export CILK_NPROC=1
-  export MKL_PARDISO_OOC_MAX_CORE_SIZE=3500
-  export MKL_PARDISO_OOC_MAX_SWAP_SIZE=2000
-
-  log_file=output_8_nodes.log
-
-  dom_size=10      #20
-  clusters=3       #2
-  domains_p_c=5    #7
-
-
-
   #               OM OK OK
   #               0   1   2   3   4   5   6   7   8   9
-  dom_size=(      20  25  21  16  16  16  16  16  13  14 )
-  clustt_size_x=( 11   10  12  10  10  10  10  10  10  10 )
+  dom_size=(      10  16  16  16  16  16  16  16  13  14 )
+  clustt_size_x=( 5  10  10  10  10  10  10  10  10  10 )
 # clustt_size_y=( 2   5   5   5   5   5   5   5   5   5 )
 # clustt_size_z=( 1   5   5   5   5   5   5   5   5   5 )
 
-  clusters_x=(    8   1   1   5   6   7   8   9   1   1 )
-  clusters_y=(    8   1   1   5   6   7   8   9   1   1 )
-  clusters_z=(    8   1   1   5   6   7   8   9   1   1 )
+  clusters_x=(    2   3   4   5   6   7   8   9   1   1 )
+  clusters_y=(    1   3   4   5   6   7   8   9   1   1 )
+  clusters_z=(    1   3   4   5   6   7   8   9   1   1 )
 
   corners=(       0   0   0   0   0   0   0   0   0   0 )
 
@@ -183,13 +163,12 @@ if [ "$1" = "run" ]; then
     #mpirun -bind-to-none -n $(( X * Y * Z ))  ./espreso ${el_type[0]} ${X} ${Y} ${Z} ${x} ${y} ${z} ${d} ${d} ${d}   | tee -a $log_file
     #mpirun -bind-to none -n $(( X * Y * Z ))  ./espreso ${el_type[0]} ${X} ${Y} ${Z} ${x} ${y} ${z} ${d} ${d} ${d}               # | tee -a $log_file
     
-    cp -R ~/espreso/libs/     	 /scratch/work/user/lriha/esp/
-    cp -R ~/espreso/examples/ 	 /scratch/work/user/lriha/esp/
-    cp    ~/espreso/espreso      /scratch/work/user/lriha/esp/
-    cp    ~/espreso/salomon.sh   /scratch/work/user/lriha/esp/
+    #cp -R ~/espreso/libs/     	 /scratch/work/user/lriha/esp/
+    #cp -R ~/espreso/examples/ 	 /scratch/work/user/lriha/esp/
+    #cp    ~/espreso/espreso      /scratch/work/user/lriha/esp/
+    #cp    ~/espreso/salomon.sh   /scratch/work/user/lriha/esp/
     
-    cd /scratch/work/user/lriha/esp/
-    
+    #cd /scratch/work/user/lriha/esp/
     
     mpirun -n $(( X * Y * Z ))  ./espreso examples/meshgenerator/cube_elasticity_fixed_bottom.txt ${el_type[0]} ${X} ${Y} ${Z} ${x} ${y} ${z} ${d} ${d} ${d}                   | tee -a $log_file
     #mpirun -n 10 ./espreso | tee -a $log_file
