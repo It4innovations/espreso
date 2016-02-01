15c597f459efe4d2f1e91a86e7faf5d44f04993e
mar440 <mar440@vsb.cz>
2016-02-01 18:18:16 +0100

    solver/src/SparseMatrix.cpp(h): grammar correction, modification related to enhanced print out of statistics of kernels etc ...

--- solver/src/SparseMatrix.h
+++ solver/src/SparseMatrix.h
@@ -164,7 +164,7 @@ public:
 	void tridiagFromCSR( SparseMatrix & A_in, char *str0);
 	double dot_e(double *x, double *y, eslocal n);
 
-	void getNorm_K_R(SparseMatrix & K, SparseMatrix &R_in_dense_format);
+	double getNorm_K_R(SparseMatrix & K, SparseMatrix &R_in_dense_format);
 	void GramSchmidtOrtho();
 
 	void MatScale(double alpha);
@@ -203,7 +203,8 @@ public:
 	void sortInCOO();
 
 //	void get_kernel_from_K();
-	void get_kernel_from_K(SparseMatrix &K, SparseMatrix &KplusR);
+	void get_kernel_from_K(SparseMatrix &K, SparseMatrix &KplusR,
+        double * norm_KR, int * defect);
 
 };
 
