a72c8f68d77937a2c2805162d48e4e3fc78f1e3e
mar440 <mar440@vsb.cz>
2016-02-01 18:08:54 +0100

    solver/src/Cluster.cpp: inputs in 'get_kernel_from_K' modified

--- solver/src/Cluster.cpp
+++ solver/src/Cluster.cpp
@@ -1273,8 +1273,11 @@ void Cluster::CreateSa() {
 		 GGt.RemoveLower();
 //		 GGt.get_kernel_from_K(GGt, Kernel_Sa);
 
+
+     double tmp_double;
+     int tmp_int;
 		 SparseMatrix TSak;
-		 Salfa.get_kernel_from_K(Salfa,Kernel_Sa);
+		 Salfa.get_kernel_from_K(Salfa,Kernel_Sa,&tmp_double,&tmp_int);
 		 TSak.Clear();
 
 		 //domains[0].get_kernel_from_K(Salfa, Kernel_Sa);
