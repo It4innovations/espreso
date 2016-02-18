404109dc3f37956fef77cf5dcc9e71fe0acddae0
lriha <lubomir.riha@vsb.cz>
2016-02-18 12:14:10 +0100

    ENH: Salomon.sh - updated run option
    Conflicts:
    	salomon.sh

--- salomon.sh
+++ salomon.sh
@@ -1,70 +1,11 @@
 #!/bin/bash
 
-#INTEL=/apps/all/icc/2015.3.187
-#. $INTEL/tbb/bin/tbbvars.sh intel64
-
-if [ "$1" = "runpbs" ]; then
-
-
-
-  export PARDISOLICMESSAGE=1
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./libs
-
-#  export OMP_NUM_THREADS=1
-#  export MKL_NUM_THREADS=1
-#  export SOLVER_NUM_THREADS=24
-
-
-export MKL_NUM_THREADS=23
-export OMP_NUM_THREADS=23
-export SOLVER_NUM_THREADS=23
-export PAR_NUM_THREADS=23
-export CILK_NWORKERS=23
-export PARDISOLICMESSAGE=1
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./libs
-export LC_CTYPE=""
-
-  dom_size=$4
-  clusters=$2
-  clustt_size=$3
-  corners=0
-
-  d=${dom_size}
-  c=${corners}
-
-  x=${clustt_size}
-  y=${clustt_size}
-  z=${clustt_size}
-
-  X=${clusters}
-  Y=${clusters}
-  Z=${clusters}
-
-  log_file=LOG-$X:$Y:$Z-$x:$y:$z-$d:$d:$d-$c:$c:$c.log
-
-  date | tee -a $log_file
-
-  echo "Config: dom_size=  $d | cluster_size = $x:$y:$z | clusters = $X:$Y:$Z  "
-
-  date | tee -a $log_file
-
-  #mpirun -n $(( X * Y * Z )) ./espreso 0 ${X} ${Y} ${Z} ${x} ${y} ${z} ${d} ${d} ${d}                   | tee -a $log_file 
-
-    cp -R ~/espreso/libs/        /scratch/work/user/lriha/esp/
-    cp -R ~/espreso/examples/    /scratch/work/user/lriha/esp/
-    cp    ~/espreso/espreso      /scratch/work/user/lriha/esp/
-    cp    ~/espreso/salomon.sh   /scratch/work/user/lriha/esp/
-
-    cd /scratch/work/user/lriha/esp/
-
- mpiexec_mpt -n $(( X * Y * Z )) perfboost -impi ./espreso examples/meshgenerator/cube_elasticity_fixed_bottom.txt 0 ${X} ${Y} ${Z} ${x} ${y} ${z} ${d} ${d} ${d}                   | tee -a $log_file
-
-#  mpirun -f /home/lriha/espreso/hostfile perfboost -impi ./espreso examples/meshgenerator/cube_elasticity_fixed_bottom.txt 0 ${X} ${Y} ${Z} ${x} ${y} ${z} ${d} ${d} ${d}                   | tee -a $log_file
-
-fi
-
-
-
+WORKDIR=~/espreso-results-mpt-3
+ESPRESODIR=~/espreso
+EXAMPLEDIR=examples/meshgenerator
+EXAMPLE=cube_elasticity_fixed_bottom.txt
+THREADS_PER_MPI=1
+MPI_PER_NODE=22
 
 #module load impi/5.0.3.048-iccifort-2015.3.187
 #module load icc/2015.3.187
@@ -80,38 +21,11 @@ module load impi/5.1.2.150-iccifort-2016.1.150-GCC-4.9.3-2.25
 module load imkl/11.3.1.150-iimpi-2016.01-GCC-4.9.3-2.25 
 module load tbb/4.4.2.152 
 
-
-#INTEL MPI
-#module load impi/5.1.2.150-iccifort-2016.1.150-GCC-4.9.3
-
-
-#START - SGI MPI
-#module load perfboost
-#module load mpt/2.12
-
-export MPI_DSM_DISTRIBUTE=0
-export MPI_SHEPHERD=1
-export PERFBOOST_VERBOSE=1
-export MPI_VERBOSE=1
-export MPI_BUFS_PER_PROC=512
-#END -  SGI MPI
-
-
-
-export MKL_NUM_THREADS=23
-export OMP_NUM_THREADS=23
-export SOLVER_NUM_THREADS=23
-export PAR_NUM_THREADS=23
-export CILK_NWORKERS=23
-export PARDISOLICMESSAGE=1
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./libs
-export LC_CTYPE=""
-
 if [ "$#" -eq 0 ]; then
   echo "  Use one of the following commands:"
   echo "    ./salomon configure"
   echo "    ./salomon build"
-  echo "    ./salomon run"
+  echo "    ./salomon run sgi|intel mpi|pbs"
   echo "    ./salomon clean"
   echo "    ./salomon distclean"
   echo ""
@@ -150,63 +64,149 @@ el_type=(   0     1      2       3       4       5        6         7)
 
 if [ "$1" = "run" ]; then
 
+  if [ "$2" = "sgi" ]; then
+    #START - SGI MPI
+    module unload impi
+    module load perfboost
+    module load mpt/2.12
+
+    export MPI_DSM_DISTRIBUTE=0
+    export MPI_SHEPHERD=1
+    export PERFBOOST_VERBOSE=1
+    export MPI_VERBOSE=1
+    export MPI_BUFS_PER_PROC=512
+    #END -  SGI MPI
+  fi
+
   #               OM OK OK
   #               0   1   2   3   4   5   6   7   8   9
-  dom_size=(      15  15  15  15  15  15  6  16  13  14 )
-  clustt_size_x=( 11  11  11  11  11  11  5  10  10  10 )
-# clustt_size_y=( 2   5   5   5   5   5   5   5   5   5 )
-# clustt_size_z=( 1   5   5   5   5   5   5   5   5   5 )
+  dom_size=(      7   15  15  15  15  15  6  16  13  14 )
+  clustt_size_x=( 6   11  11  11  11  11  5  10  10  10 )
+  clustt_size_y=( 2   5   5   5   5   5   5   5   5   5 )
+  clustt_size_z=( 1   5   5   5   5   5   5   5   5   5 )
 
-  clusters_x=(    2   3   4   5   6   7   8  10   10   1 )
-  clusters_y=(    2   3   4   5   6   7   8   9   10   1 )
-  clusters_z=(    2   3   4   5   6   7   8   9   10   1 )
+  clusters_x=(    4   3   4   5   6   7   8  10   10   1 )
+  clusters_y=(    4   3   4   5   6   7   8   9   10   1 )
+  clusters_z=(    4   3   4   5   6   7   8   9   10   1 )
 
   corners=(       0   0   0   0   0   0   0   0   0   0 )
 
-  for i in 7 # 0 1 2 3 4 5 6
+  qsub_command_0="#!/bin/bash;"
+  qsub_command_0+="export MKL_NUM_THREADS=1;"
+  qsub_command_0+="export OMP_NUM_THREADS=1;"
+  qsub_command_0+="export SOLVER_NUM_THREADS=$THREADS_PER_MPI;"
+  qsub_command_0+="export PAR_NUM_THREADS=$THREADS_PER_MPI;"
+  qsub_command_0+="export CILK_NWORKERS=$THREADS_PER_MPI;"
+  qsub_command_0+="export PARDISOLICMESSAGE=1;"
+  qsub_command_0+="export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./libs:.;"
+  qsub_command_0+="export LC_CTYPE=;"
+
+  if [ "$2" = "intel" ]; then
+    qsub_command_0+="module load impi/5.1.2.150-iccifort-2016.1.150-GCC-4.9.3-2.25;"
+  fi
+
+  qsub_command_0+="module load imkl/11.3.1.150-iimpi-2016.01-GCC-4.9.3-2.25;"
+  qsub_command_0+="module load tbb/4.4.2.152;"
+
+  if [ "$2" = "sgi" ]; then
+    qsub_command_0+="module unload impi;"
+    qsub_command_0+="module unload iimpi;"
+
+    qsub_command_0+="module load perfboost;"
+    qsub_command_0+="module load mpt/2.12;"
+
+    qsub_command_0+="export MPI_DSM_DISTRIBUTE=0;"
+    qsub_command_0+="export MPI_SHEPHERD=1;"
+    qsub_command_0+="export PERFBOOST_VERBOSE=1;"
+    qsub_command_0+="export MPI_VERBOSE=1;"
+    qsub_command_0+="export MPI_BUFS_PER_PROC=512;"
+  fi
+  
+  qsub_command_0+="module list;"
+	
+  for i in 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 # 22 23 24 25 26 27 28 29 30 31
   do
     d=${dom_size[${i}]}
     c=${corners[${i}]}
 
     x=${clustt_size_x[${i}]}
-    y=${clustt_size_x[${i}]}
-    z=${clustt_size_x[${i}]}
+    y=${clustt_size_y[${i}]}
+    z=${clustt_size_z[${i}]}
 
     X=${clusters_x[${i}]}
     Y=${clusters_y[${i}]}
     Z=${clusters_z[${i}]}
 
-    log_file=LOG-$X:$Y:$Z-$x:$y:$z-$d:$d:$d-$c:$c:$c.log
+    d=7
+    c=0
+    x=6
 
-    date | tee -a $log_file
+    y=$x
+    z=$x
+
+    X=$i
+    Y=$X
+    Z=$X
+
+    jobname=espreso
+    mpiranks=$(( X * Y * Z ))
+    account=SERVICE
+    actualTime=$( date +%y%m%d_%H:%M:%S )
+    log_file=LOG-$X:$Y:$Z-$x:$y:$z-$d:$d:$d-$c:$c:$c-$actualTime.log
+    out_dir=ESP-$X:$Y:$Z-$x:$y:$z-$d:$d:$d-$c:$c:$c-$actualTime
+    qsub_command=$qsub_command_0
+    qsub_command+="date | tee -a $log_file;"
 
     echo "Config: dom_size=  $d | cluster_size = $x:$y:$z | clusters = $X:$Y:$Z  "
+    mkdir    $WORKDIR/$out_dir
+    mkdir -p $WORKDIR/$out_dir/$EXAMPLEDIR
+    cp -R    $ESPRESODIR/libs/          $WORKDIR/$out_dir
+    cp -R    $ESPRESODIR/$EXAMPLEDIR/*  $WORKDIR/$out_dir/$EXAMPLEDIR
+    cp       $ESPRESODIR/espreso        $WORKDIR/$out_dir
+    cp       $ESPRESODIR/salomon.sh     $WORKDIR/$out_dir
 
-    date | tee -a $log_file
+    qsub_command+="cd $WORKDIR/$out_dir;"
+    qsub_command+="ls;"
 
-    #                                          ./espreso ${el_type[0]} ${X} ${Y} ${Z} ${x} ${y} ${z} ${d} ${d} ${d}   | tee -a $log_file
-    #mpirun -bind-to-none -n $(( X * Y * Z ))  ./espreso ${el_type[0]} ${X} ${Y} ${Z} ${x} ${y} ${z} ${d} ${d} ${d}   | tee -a $log_file
-    #mpirun -bind-to none -n $(( X * Y * Z ))  ./espreso ${el_type[0]} ${X} ${Y} ${Z} ${x} ${y} ${z} ${d} ${d} ${d}               # | tee -a $log_file
+    if [ "$3" = "mpi" ]; then
+
+      export MKL_NUM_THREADS=1
+      export OMP_NUM_THREADS=1
+      export SOLVER_NUM_THREADS=$THREADS_PER_MPI
+      export PAR_NUM_THREADS=$THREADS_PER_MPI
+      export CILK_NWORKERS=$THREADS_PER_MPI
+      export PARDISOLICMESSAGE=1
+      export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./libs:.
+      export LC_CTYPE=
+
+      if [ "$2" = "intel" ]; then
+        cd $WORKDIR/$out_dir
+        mpirun      -n $(( X * Y * Z ))                  ./espreso examples/meshgenerator/cube_elasticity_fixed_bottom.txt ${el_type[0]} ${X} ${Y} ${Z} ${x} ${y} ${z} ${d} ${d} ${d}                   | tee -a $log_file
+      fi
+      
+      if [ "$2" = "sgi" ]; then
+        cd $WORKDIR/$out_dir
+        mpiexec_mpt -n $(( X * Y * Z )) perfboost -impi  ./espreso examples/meshgenerator/cube_elasticity_fixed_bottom.txt ${el_type[0]} ${X} ${Y} ${Z} ${x} ${y} ${z} ${d} ${d} ${d}                   | tee -a $log_file
+      fi
+    fi
+
+
+    if [ "$3" = "pbs" ]; then
+      if [ "$2" = "sgi" ]; then
+        qsub_command+="mpiexec_mpt -n $(( X * Y * Z )) perfboost -impi ./espreso $EXAMPLEDIR/$EXAMPLE ${el_type[0]} ${X} ${Y} ${Z} ${x} ${y} ${z} ${d} ${d} ${d} | tee -a $log_file;"
+      fi
+      if [ "$2" = "intel" ]; then
+        qsub_command+="mpirun      -n $(( X * Y * Z ))                 ./espreso $EXAMPLEDIR/$EXAMPLE ${el_type[0]} ${X} ${Y} ${Z} ${x} ${y} ${z} ${d} ${d} ${d} | tee -a $log_file;"    
+      fi
+ 
+      echo $qsub_command | tr ";" "\n" | tee $WORKDIR/$out_dir/job.qsub
+      echo "qsub -q qmpp -A $account -l select=$(( ((X * Y * Z)/MPI_PER_NODE) + 1 )):ncpus=24:mpiprocs=$MPI_PER_NODE:ompthreads=$THREADS_PER_MPI -l walltime=00:20:00 -N $jobname" | tee $WORKDIR/$out_dir/job.qsub
+
+      echo $qsub_command | tr ";" "\n" | \
+      qsub -q qmpp -A $account -l select=$(( ((X * Y * Z)/MPI_PER_NODE) + 1 )):ncpus=24:mpiprocs=$MPI_PER_NODE:ompthreads=$THREADS_PER_MPI -l walltime=00:20:00 -N $jobname
     
-    cp -R ~/espreso/libs/     	 /scratch/work/user/lriha/esp/
-    cp -R ~/espreso/examples/ 	 /scratch/work/user/lriha/esp/
-    cp    ~/espreso/espreso      /scratch/work/user/lriha/esp/
-    cp    ~/espreso/salomon.sh   /scratch/work/user/lriha/esp/
-    
-    cd /scratch/work/user/lriha/esp/
+    fi
 
-    #mpirun -n $(( X * Y * Z )) hostname
-    #mpirun -n $(( X * Y * Z ))  ./espreso examples/meshgenerator/cube_elasticity_fixed_bottom.txt ${el_type[0]} ${X} ${Y} ${Z} ${x} ${y} ${z} ${d} ${d} ${d}                   | tee -a $log_file
-    
-
-    mpiexec_mpt -n $(( X * Y * Z )) perfboost -impi ./espreso examples/meshgenerator/cube_elasticity_fixed_bottom.txt ${el_type[0]} ${X} ${Y} ${Z} ${x} ${y} ${z} ${d} ${d} ${d}                   | tee -a $log_file
-
-    #mpirun -n 10 ./espreso | tee -a $log_file
-   
-    #ddt -noqueue -start -n $(( X * Y * Z ))    ./espreso ${el_type[0]} ${X} ${Y} ${Z} ${x} ${y} ${z} ${d} ${d} ${d} # | tee -a $log_file
-   	
-    #cp mesh.vtk mesh-$X:$Y:$Z-$x:$y:$z-$d:$d:$d-$c:$c:$c.vtk
-    #rm mesh.vtk
   done
 
 fi
