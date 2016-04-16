56aedc053e8a188ed406fff7696419e9605c6093
Lubomir Riha <lubomir.riha@vsb.cz>
2016-04-16 09:28:44 -0400

    stav pro testovani

--- config/esconfig.cpp
+++ config/esconfig.cpp
@@ -15,7 +15,7 @@ namespace mesh {
 	bool edgeCorners = false;
 	bool faceCorners = false;
 
-	bool averageEdges = true;
+	bool averageEdges = false;
 	bool averageFaces = false;
 
 	Input input = GENERATOR;
@@ -28,15 +28,15 @@ namespace assembler {
 }
 namespace solver {
 
-	double 		epsilon 				= 0.0001;	// Solver requested precision
+	double 		epsilon 			= 0.001;	// Solver requested precision
 	size_t 		maxIterations			= 200;		//
-	size_t   	FETI_METHOD				= 1; 		// 0 - Total FETI; 1 - HFETI;
-	size_t   	USE_SCHUR_COMPLEMENT	= 0; 		// 1 - YES
+	size_t   	FETI_METHOD			= 1; 		// 0 - Total FETI; 1 - HFETI;
+	size_t   	USE_SCHUR_COMPLEMENT		= 0; 		// 1 - YES
 	size_t		KEEP_FACTORS			= 1; 		// 1 - YES; 0 - NO
-	size_t   	PRECONDITIONER			= 0;		// 0 - NO preconditioner; 1 - Lumped; 2 - weight function;
-	size_t		CG_SOLVER				= 0;		// 0 - Standard CG; 1 - Pipelined CG
-	size_t		REGULARIZATION 			= 1;		// 0 - from mesh; 1 - from stifness matrix
-	size_t		KSOLVER					= 0;		// 0 - Direct, 1 - Iter
+	size_t   	PRECONDITIONER			= 1;		// 0 - NO preconditioner; 1 - Lumped; 2 - weight function;
+	size_t		CG_SOLVER			= 0;		// 0 - Standard CG; 1 - Pipelined CG
+	size_t		REGULARIZATION 			= 0;		// 0 - from mesh; 1 - from stifness matrix
+	size_t		KSOLVER				= 0;		// 0 - Direct, 1 - Iter
 
 
 }
