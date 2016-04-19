ecf53ecb1aed042db89a7cf80ebb55dcd6c3b9ce
Radim Vavrik <radim.vavrik@vsb.cz>
2016-04-19 14:38:11 +0200

    SparseSolverCPU defined as SparseSolverCUDA

--- solver/specific/sparsesolvers.h
+++ solver/specific/sparsesolvers.h
@@ -43,7 +43,7 @@ namespace espreso {
 #include "acc/solvercuda.h"
 
 namespace espreso {
-	typedef SparseSolverMKL SparseSolverCPU;
+	typedef SparseSolverCUDA SparseSolverCPU;
 	typedef SparseSolverCUDA SparseSolverAcc;
 }
 
