cc48ec0f6049c40aed5815068548dd72d71e58c2
mec059 <ondrej.meca@vsb.cz>
2016-04-28 10:26:37 +0200

    FIX: add forgotten file

---
+++ mesh/structures/material.h
@@ -0,0 +1,15 @@
+
+#ifndef MESH_STRUCTURES_MATERIAL_H_
+#define MESH_STRUCTURES_MATERIAL_H_
+
+struct Material {
+
+	double density;
+
+	double youngModulus;
+	double poissonRatio;
+};
+
+
+
+#endif /* MESH_STRUCTURES_MATERIAL_H_ */
