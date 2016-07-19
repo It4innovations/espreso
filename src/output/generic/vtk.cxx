958f67313d1b873313ee017e1aff7d17ecd0bcc8
mec059 <ondrej.meca@vsb.cz>
2016-07-19 15:48:38 +0200

    ENH: yellow warning for VTK formats

--- src/output/generic/vtk.cxx
+++ src/output/generic/vtk.cxx
@@ -9,7 +9,12 @@ using namespace espreso::output;
 
 Generic::Generic(const Mesh &mesh, const std::string &path): ResultStore(mesh, path)
 {
-	ESINFO(ALWAYS) << "Warning: ESPRESO not contains a library for saving generic VTK format. VTK legacy format is used.";
+	switch (config::output::OUTPUT_FORMAT) {
+	case config::output::OUTPUT_FORMATAlternatives::VTK_LEGACY:
+		break;
+	default:
+		ESINFO(ALWAYS) << TextColor::YELLOW << "Warning: ESPRESO not contains a library for saving generic VTK format. VTK Legacy format is used.";
+	}
 }
 
 void Generic::store(double shrinkSubdomain, double shrinkCluster)
@@ -22,5 +27,4 @@ void Generic::store(std::vector<std::vector<double> > &displacement, double shri
 {
 	VTK_Full output(_mesh, _path);
 	output.store(displacement, shrinkSubdomain, shrinkCluster);
-
 }
