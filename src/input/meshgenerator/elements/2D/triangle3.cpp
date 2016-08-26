b4cac70db448f66963ad68a6c92ad8f53e77787e
mec059 <ondrej.meca@vsb.cz>
2016-08-26 08:19:30 +0200

    ENH: add function addEdges and pickNodes for the rest of plane elements

--- src/input/meshgenerator/elements/2D/triangle3.cpp
+++ src/input/meshgenerator/elements/2D/triangle3.cpp
@@ -21,5 +21,53 @@ void Triangle3::addElements(std::vector<Element*> &elements, const eslocal indic
 	elements.push_back(new espreso::Triangle3(triangle, params));
 }
 
+void Triangle3::addEdges(std::vector<Element*> &edges, const eslocal indices[], CubeEdges edge)
+{
+	eslocal line[2];
+	switch (edge) {
+	case CubeEdges::X_0_Z_0:
+		line[0] = indices[2];
+		line[1] = indices[0];
+		break;
+	case CubeEdges::X_1_Z_0:
+		line[0] = indices[1];
+		line[1] = indices[3];
+		break;
+	case CubeEdges::Y_0_Z_0:
+		line[0] = indices[0];
+		line[1] = indices[1];
+		break;
+	case CubeEdges::Y_1_Z_0:
+		line[0] = indices[3];
+		line[1] = indices[2];
+		break;
+	default:
+		ESINFO(GLOBAL_ERROR) << "Unknown edge";
+	}
+	edges.push_back(new espreso::Line2(line));
+}
 
+void Triangle3::pickNodes(const std::vector<Element*> &nodes, std::vector<Element*> &selection, const eslocal indices[], CubeEdges edge)
+{
+	switch (edge) {
+	case CubeEdges::X_0_Z_0:
+		selection.push_back(nodes[indices[2]]);
+		selection.push_back(nodes[indices[0]]);
+		break;
+	case CubeEdges::X_1_Z_0:
+		selection.push_back(nodes[indices[1]]);
+		selection.push_back(nodes[indices[3]]);
+		break;
+	case CubeEdges::Y_0_Z_0:
+		selection.push_back(nodes[indices[0]]);
+		selection.push_back(nodes[indices[1]]);
+		break;
+	case CubeEdges::Y_1_Z_0:
+		selection.push_back(nodes[indices[3]]);
+		selection.push_back(nodes[indices[2]]);
+		break;
+	default:
+		ESINFO(GLOBAL_ERROR) << "Unknown edge";
+	}
+}
 
--- src/input/meshgenerator/elements/2D/triangle6.cpp
+++ src/input/meshgenerator/elements/2D/triangle6.cpp
@@ -29,5 +29,61 @@ void Triangle6::addElements(std::vector<Element*> &elements, const eslocal indic
 	elements.push_back(new espreso::Triangle6(triangle, params));
 }
 
+void Triangle6::addEdges(std::vector<Element*> &edges, const eslocal indices[], CubeEdges edge)
+{
+	eslocal line[3];
+	switch (edge) {
+	case CubeEdges::X_0_Z_0:
+		line[0] = indices[6];
+		line[1] = indices[3];
+		line[2] = indices[0];
+		break;
+	case CubeEdges::X_1_Z_0:
+		line[0] = indices[2];
+		line[1] = indices[5];
+		line[2] = indices[8];
+		break;
+	case CubeEdges::Y_0_Z_0:
+		line[0] = indices[0];
+		line[1] = indices[1];
+		line[2] = indices[2];
+		break;
+	case CubeEdges::Y_1_Z_0:
+		line[0] = indices[8];
+		line[1] = indices[7];
+		line[2] = indices[6];
+		break;
+	default:
+		ESINFO(GLOBAL_ERROR) << "Unknown edge";
+	}
+	edges.push_back(new espreso::Line3(line));
+}
 
+void Triangle6::pickNodes(const std::vector<Element*> &nodes, std::vector<Element*> &selection, const eslocal indices[], CubeEdges edge)
+{
+	switch (edge) {
+	case CubeEdges::X_0_Z_0:
+		selection.push_back(nodes[indices[6]]);
+		selection.push_back(nodes[indices[3]]);
+		selection.push_back(nodes[indices[0]]);
+		break;
+	case CubeEdges::X_1_Z_0:
+		selection.push_back(nodes[indices[2]]);
+		selection.push_back(nodes[indices[5]]);
+		selection.push_back(nodes[indices[8]]);
+		break;
+	case CubeEdges::Y_0_Z_0:
+		selection.push_back(nodes[indices[0]]);
+		selection.push_back(nodes[indices[1]]);
+		selection.push_back(nodes[indices[2]]);
+		break;
+	case CubeEdges::Y_1_Z_0:
+		selection.push_back(nodes[indices[8]]);
+		selection.push_back(nodes[indices[7]]);
+		selection.push_back(nodes[indices[6]]);
+		break;
+	default:
+		ESINFO(GLOBAL_ERROR) << "Unknown edge";
+	}
+}
 
