fa31a717295898f8f80dd0d32928986514612a23
mec059 <ondrej.meca@vsb.cz>
2017-01-16 12:30:14 +0100

    FIX: B0 for advection diffusion 3D

--- src/assembler/physics/linear/advectiondiffusion3d/assembler.cpp
+++ src/assembler/physics/linear/advectiondiffusion3d/assembler.cpp
@@ -59,7 +59,7 @@ void AdvectionDiffusion3D::assembleB0()
 			break;
 		case config::solver::B0_TYPEalternative::KERNELS:
 			std::for_each(R1.begin(), R1.end(), [] (SparseMatrix &m) { m.ConvertCSRToDense(0); });
-			EqualityConstraints::insertKernelsToB0(_constraints, _mesh.edges(), pointDOFs, R1);
+			EqualityConstraints::insertKernelsToB0(_constraints, _mesh.faces(), pointDOFs, R1);
 			break;
 		default:
 			break;
