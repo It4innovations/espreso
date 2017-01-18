8d839101a07b0db3ae4c4d9d2fe7ef257aa9b8d0
mec059 <ondrej.meca@vsb.cz>
2017-01-18 12:51:14 +0100

    ENH: add tests to load OpenFOAM

---
+++ tests/input.py
@@ -0,0 +1,26 @@
+
+from utils import *
+import unittest
+import glob
+import string
+
+ESPRESO_TESTS = os.path.dirname(os.path.abspath(__file__))
+ROOT = os.path.dirname(ESPRESO_TESTS)
+
+class ESPRESOInput(unittest.TestCase):
+
+    espreso = Espreso()
+
+    def openfoam(self, path, file):
+        config = { "ENV::TESTING_LEVEL": 0, "ENV::VERBOSE_LEVEL": 0, "ENV::MEASURE_LEVEL": 0, "OUTPUT::RESULTS": 0 }
+        self.espreso.run(len(glob.glob(path + "/processor*")), path, config)
+
+if __name__ == '__main__':
+
+    openfoam  = os.path.join(ESPRESO_TESTS, "examples", "input", "openfoam")
+    workbench = os.path.join(ESPRESO_TESTS, "examples", "input", "workbench")
+
+    for name, path, file in TestCaseCreator.gather(openfoam, ".ecf"):
+        TestCaseCreator.create_test(ESPRESOInput, ESPRESOInput.openfoam, "openfoam_" + name, path, file)
+
+    unittest.main()
