f6dc463edd41ad2266443ca1d83c85aa18bd1d6c
mec059 <ondrej.meca@vsb.cz>
2017-01-23 09:25:09 +0100

    ENH: add workbench test

--- tests/input.py
+++ tests/input.py
@@ -15,6 +15,10 @@ class ESPRESOInput(unittest.TestCase):
         config = { "ENV::TESTING_LEVEL": 0, "ENV::VERBOSE_LEVEL": 0, "ENV::MEASURE_LEVEL": 0, "OUTPUT::RESULTS": 0 }
         self.espreso.run(len(glob.glob(path + "/processor*")), path, config)
 
+    def workbench(self, path, file):
+        config = { "ENV::TESTING_LEVEL": 0, "ENV::VERBOSE_LEVEL": 0, "ENV::MEASURE_LEVEL": 0, "OUTPUT::RESULTS": 0 }
+        self.espreso.run(1, path, config)
+
 if __name__ == '__main__':
 
     openfoam  = os.path.join(ESPRESO_TESTS, "examples", "input", "openfoam")
@@ -22,5 +26,8 @@ if __name__ == '__main__':
 
     for name, path, file in TestCaseCreator.gather(openfoam, ".ecf"):
         TestCaseCreator.create_test(ESPRESOInput, ESPRESOInput.openfoam, "openfoam_" + name, path, file)
+
+    for name, path, file in TestCaseCreator.gather(workbench, ".ecf"):
+        TestCaseCreator.create_test(ESPRESOInput, ESPRESOInput.workbench, "workbench_" + name, path, file)
 
     unittest.main()
