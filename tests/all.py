8be706723d5347432c02887da172f1eefac7b237
mec059 <ondrej.meca@vsb.cz>
2017-01-30 10:24:45 +0100

    RF: fix printing in all tests

--- tests/all.py
+++ tests/all.py
@@ -5,9 +5,11 @@ ESPRESO_TESTS = os.path.dirname(os.path.abspath(__file__))
 ROOT = os.path.dirname(ESPRESO_TESTS)
 
 if __name__ == '__main__':
-    print "\nBENCHMARKS TESTS:"
+    os.system("echo \"\nBENCHMARKS TESTS:\"")
     os.system("python " + os.path.join(ROOT, "tests", "benchmarks.py"))
-    print "\nINPUT TESTS:"
+
+    os.system("echo \"\nINPUT TESTS:\"")
     os.system("python " + os.path.join(ROOT, "tests", "input.py"))
-    print "\nSOLVER TESTS:"
+
+    os.system("echo \"\nSOLVER TESTS:\"")
     os.system("python " + os.path.join(ROOT, "tests", "solver.py"))
