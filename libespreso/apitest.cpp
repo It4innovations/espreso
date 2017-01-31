c938e7d3e3945c2c022fe13c0e33fe024102d54e
mec059 <ondrej.meca@vsb.cz>
2017-01-31 14:15:17 +0100

    RF: again build API test

--- libespreso/apitest.cpp
+++ libespreso/apitest.cpp
@@ -4,6 +4,14 @@
 // we use ESPRESO assembler for compute stiffness matrices
 #include "../src/app/factory/factory.h"
 #include "../src/config/globalconfiguration.h"
+#include "../src/basis/matrices/denseMatrix.h"
+#include "../src/output/vtk/vtk.h"
+#include "../src/mesh/settings/property.h"
+#include "../src/mesh/elements/element.h"
+#include "../src/mesh/structures/mesh.h"
+#include "../src/mesh/structures/coordinates.h"
+#include "../src/assembler/instance/instance.h"
+#include "../src/assembler/physics/assembler.h"
 
 int main(int argc, char** argv)
 {
@@ -18,7 +26,7 @@ int main(int argc, char** argv)
 
 	// use ESPRESO factory to allow run test on all examples/assemblers
 	espreso::Factory factory(configuration);
-	factory.mesh.partitiate(1); // we need only one sub-domain per cluster
+	factory.mesh->partitiate(1); // we need only one sub-domain per cluster
 	factory.instance->init(); // it perform full initialization; it is not effective but sufficient to test API
 
 	std::vector<espreso::Property> DOFs = factory.instance->physics().pointDOFs;
@@ -27,19 +35,19 @@ int main(int argc, char** argv)
 	std::vector<FETI4IInt> l2g(DOFsSize);
 
 	// TODO: generalize l2g and dirichlet
-	for (size_t n = 0; n < factory.mesh.coordinates().clusterSize(); n++) {
+	for (size_t n = 0; n < factory.mesh->coordinates().clusterSize(); n++) {
 		for (size_t dof = 0; dof < DOFs.size(); dof++) {
-			l2g[DOFs.size() * n + dof] = DOFs.size() * factory.mesh.coordinates().globalIndex(n) + dof;
+			l2g[DOFs.size() * n + dof] = DOFs.size() * factory.mesh->coordinates().globalIndex(n) + dof;
 		}
 	}
 
 	std::vector<FETI4IInt> dirichletIndices;
 	std::vector<FETI4IReal> dirichletValues;
-	for (size_t n = 0; n < factory.mesh.nodes().size(); n++) {
+	for (size_t n = 0; n < factory.mesh->nodes().size(); n++) {
 		for (size_t dof = 0; dof < DOFs.size(); dof++) {
-			if (factory.mesh.nodes()[n]->hasProperty(DOFs[dof], 0)) {
+			if (factory.mesh->nodes()[n]->hasProperty(DOFs[dof], 0)) {
 				dirichletIndices.push_back(DOFs.size() * n + dof);
-				dirichletValues.push_back(factory.mesh.nodes()[n]->getProperty(DOFs[dof], n, 0, 0));
+				dirichletValues.push_back(factory.mesh->nodes()[n]->getProperty(DOFs[dof], n, 0, 0));
 			}
 		}
 	}
@@ -51,21 +59,21 @@ int main(int argc, char** argv)
 	FETI4ICreateStiffnessMatrix(&K, (int)factory.instance->physics().mtype, indexBase);
 
 	std::vector<FETI4IReal> rhs(factory.instance->physics().f[0]);
-	std::vector<FETI4IMPIInt> neighbours(factory.mesh.neighbours());
+	std::vector<FETI4IMPIInt> neighbours(factory.mesh->neighbours());
 
 	// Compose the matrix from elements matrices
-	for (size_t e = 0; e < factory.mesh.elements().size(); e++) {
-		FETI4IInt dimension = static_cast<FETI4IInt>(factory.mesh.elements()[e]->type());
+	for (size_t e = 0; e < factory.mesh->elements().size(); e++) {
+		FETI4IInt dimension = static_cast<FETI4IInt>(factory.mesh->elements()[e]->type());
 		std::vector<FETI4IInt> nodes;
 		std::vector<FETI4IInt> dofs;
 		espreso::DenseMatrix Ke;
 		std::vector<double> fe;
 
-		for (size_t n = 0; n < factory.mesh.elements()[e]->nodes(); n++) {
-			nodes.push_back(factory.mesh.elements()[e]->node(n));
+		for (size_t n = 0; n < factory.mesh->elements()[e]->nodes(); n++) {
+			nodes.push_back(factory.mesh->elements()[e]->node(n));
 		}
 
-		factory.instance->physics().assembleStiffnessMatrix(factory.mesh.elements()[e], Ke, fe, dofs);
+		factory.instance->physics().assembleStiffnessMatrix(factory.mesh->elements()[e], Ke, fe, dofs);
 		FETI4IAddElement(K, dimension, nodes.size(), nodes.data(), dofs.size(), dofs.data(), Ke.values());
 	}
 
@@ -110,7 +118,7 @@ int main(int argc, char** argv)
 
 	// Process solution
 
-	espreso::store::VTK vtk(configuration.output, factory.mesh, "results");
+	espreso::store::VTK vtk(configuration.output, *factory.mesh, "results");
 	vtk.storeGeometry();
 	vtk.storeValues("api_result", DOFs.size(), solution, espreso::store::ResultStore::ElementType::NODES);
 
