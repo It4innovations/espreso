be70c3702ac9eb8679591948679fdf955c487891
Ondrej Meca <ondrej.meca@gmail.com>
2017-02-22 18:30:39 +0100

    ENH: instances can share matrices

--- src/assembler/instance.cpp
+++ src/assembler/instance.cpp
@@ -7,7 +7,20 @@
 using namespace espreso;
 
 Instance::Instance(size_t domains, const std::vector<int> &neighbours)
-: domains(domains), neighbours(neighbours)
+: domains(domains),
+  neighbours(neighbours),
+  DOFs(_DOFs),
+  K(_K), N1(_N1), N2(_N2), RegMat(_RegMat),
+  M(_M),
+  R(_R), f(_f),
+  B0(_B0),
+  B0subdomainsMap(_B0subdomainsMap),
+  B1(_B1),
+  B1subdomainsMap(_B1subdomainsMap),
+  B1clustersMap(_B1clustersMap),
+  B1c(_B1c), LB(_LB), B1duplicity(_B1duplicity),
+  inequality(_inequality), inequalityC(_inequalityC),
+  block(_block)
 {
 	DOFs.resize(domains);
 
@@ -15,8 +28,10 @@ Instance::Instance(size_t domains, const std::vector<int> &neighbours)
 	N1.resize(domains);
 	N2.resize(domains);
 	RegMat.resize(domains);
-	f.resize(domains);
+
+	M.resize(domains);
 	R.resize(domains);
+	f.resize(domains);
 
 	B0.resize(domains);
 	B0subdomainsMap.resize(domains);
@@ -42,6 +57,91 @@ Instance::Instance(size_t domains, const std::vector<int> &neighbours)
 		B1[d].cols = 0;
 		B1[d].nnz = 0;
 		B1[d].type = 'G';
+	}
+}
+
+Instance::Instance(Instance &other, Matrices &share)
+: domains(other.domains),
+  neighbours(other.neighbours),
+
+  // shared K -> also share kernels and regularization matrix
+  DOFs(share & Matrices::K ? other.DOFs :_DOFs),
+  K(share & Matrices::K ? other.K : _K),
+  N1(share & Matrices::K ? other.N1 : _N1),
+  N2(share & Matrices::K ? other.N2 : _N2),
+  RegMat(share & Matrices::K ? other.RegMat : _RegMat),
+
+  M(share & Matrices::M ? other.M : _M),
+  R(share & Matrices::R ? other.R : _R),
+  f(share & Matrices::f ? other.f : _f),
+
+  B0(share & Matrices::B0 ? other.B0 : _B0),
+  B0subdomainsMap(share & Matrices::B0 ? other.B0subdomainsMap : _B0subdomainsMap),
+
+  B1(share & Matrices::B1 ? other.B1 : _B1),
+  B1subdomainsMap(share & Matrices::B1 ? other.B1subdomainsMap : _B1subdomainsMap),
+  B1clustersMap(share & Matrices::B1 ? other.B1clustersMap : _B1clustersMap),
+  B1c(share & Matrices::B1c ? other.B1c : _B1c),
+  LB(share & Matrices::B1 ? other.LB : _LB),
+  B1duplicity(share & (Matrices::B1 | Matrices::K) ? other.B1duplicity : _B1duplicity),
+  inequality(share & Matrices::B1 ? other.inequality : _inequality),
+  inequalityC(share & Matrices::B1 ? other.inequalityC : _inequalityC),
+  block(share & Matrices::B1 ? other.block : _block)
+{
+	if (!(share & Matrices::K)) {
+		DOFs.resize(domains);
+		K.resize(domains);
+		N1.resize(domains);
+		N2.resize(domains);
+		RegMat.resize(domains);
+	}
+
+	if (!(share & Matrices::M)) {
+		M.resize(domains);
+	}
+	if (!(share & Matrices::R)) {
+		R.resize(domains);
+	}
+	if (!(share & Matrices::f)) {
+		f.resize(domains);
+	}
+
+	if (!(share & Matrices::B0)) {
+		B0.resize(domains);
+		B0subdomainsMap.resize(domains);
+
+		for (size_t d = 0; d < domains; d++) {
+			B0[d].rows = 0;
+			B0[d].cols = 0;
+			B0[d].nnz = 0;
+			B0[d].type = 'G';
+		}
+	}
+
+	if (!(share & Matrices::B1)) {
+		B1.resize(domains);
+		B1subdomainsMap.resize(domains);
+		LB.resize(domains);
+
+		inequality.resize(domains);
+		inequalityC.resize(domains);
+
+		block.resize(3);
+
+		for (size_t d = 0; d < domains; d++) {
+			B1[d].rows = 0;
+			B1[d].cols = 0;
+			B1[d].nnz = 0;
+			B1[d].type = 'G';
+		}
+	}
+
+	if (!(share & Matrices::B1c)) {
+		B1c.resize(domains);
+	}
+
+	if (!((share & Matrices::B1c) & (share & Matrices::K))) {
+		B1duplicity.resize(domains);
 	}
 }
 
--- src/assembler/instance.h
+++ src/assembler/instance.h
@@ -26,30 +26,30 @@ enum Matrices : int {
 struct Instance {
 
 	Instance(size_t domains, const std::vector<int> &neighbours);
+	Instance(Instance &other, Matrices &share);
 	~Instance();
 
 	size_t domains;
 	std::vector<int> neighbours;
-	std::vector<size_t> DOFs;
+	std::vector<size_t> &DOFs;
 
-	std::vector<SparseMatrix> K, M, N1, N2, RegMat;
-	std::vector<std::vector<double> > R, f;
+	std::vector<SparseMatrix> &K, &N1, &N2, &RegMat;
+	std::vector<SparseMatrix> &M;
+	std::vector<std::vector<double> > &R, &f;
 
 	// matrices for Hybrid FETI constraints
-	std::vector<SparseMatrix> B0;
-	std::vector<std::vector<esglobal> > B0subdomainsMap; // TODO: not needed
+	std::vector<SparseMatrix> &B0;
+	std::vector<std::vector<esglobal> > &B0subdomainsMap; // TODO: not needed
 
 	// matrices for FETI constraints
-	std::vector<SparseMatrix> B1;
-	std::vector<std::vector<esglobal> > B1subdomainsMap; // TODO: not needed
-	std::vector<std::vector<esglobal> > B1clustersMap; // TODO: get it directly
+	std::vector<SparseMatrix> &B1;
+	std::vector<std::vector<esglobal> > &B1subdomainsMap; // TODO: not needed
+	std::vector<std::vector<esglobal> > &B1clustersMap; // TODO: get it directly
 
-	std::vector<std::vector<double> > B1c;
-	std::vector<std::vector<double> > LB;
-	std::vector<std::vector<double> > B1duplicity;
+	std::vector<std::vector<double> > &B1c, &LB, &B1duplicity;
 
-	std::vector<SparseMatrix> inequality;
-	std::vector<std::vector<double> > inequalityC;
+	std::vector<SparseMatrix> &inequality;
+	std::vector<std::vector<double> > &inequalityC;
 
 	// blocks types of B1
 	enum CONSTRAINT {
@@ -58,13 +58,23 @@ struct Instance {
 		INEQUALITY_CONSTRAINTS,
 	};
 
-	std::vector<size_t> block;
+	std::vector<size_t> &block;
 
 
 	std::vector<std::vector<double> > primalSolution;
 	std::vector<std::vector<double> > dualSolution;
 
 	std::vector<Solution*> solutions;
+
+private:
+	std::vector<SparseMatrix> _K, _M, _N1, _N2, _RegMat;
+	std::vector<std::vector<double> > _R, _f;
+
+	std::vector<SparseMatrix> _B0, _B1, _inequality;
+	std::vector<std::vector<double> > _B1c, _LB, _B1duplicity, _inequalityC;
+	std::vector<std::vector<esglobal> > _B0subdomainsMap, _B1subdomainsMap, _B1clustersMap;
+
+	std::vector<size_t> _DOFs, _block;
 };
 
 inline Matrices operator~(Matrices m)
