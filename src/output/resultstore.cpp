93a799379cd7977057f53cf031ea1d03271468af
mec059 <ondrej.meca@vsb.cz>
2017-05-29 11:55:04 +0200

    FIX: store gluing matrix - B1

--- src/output/resultstore.cpp
+++ src/output/resultstore.cpp
@@ -207,7 +207,6 @@ void ResultStore::storeFETIData(const Step &step, const Instance &instance)
 	storeElementInfo(step);
 	storeFixPoints(step);
 	storeCorners(step);
-	return; // FIX STORING B1
 	storeDirichlet(step, instance);
 	storeLambdas(step, instance);
 }
@@ -252,14 +251,22 @@ void ResultStore::storeCorners(const Step &step)
 
 void ResultStore::storeDirichlet(const Step &step, const Instance &instance)
 {
-	MeshInfo *info = _meshInfo->copyWithoutMesh();
+	if (!_meshInfo->distributed()) {
+		// TODO: send dirichlet to process 0
+		return;
+	}
+
 	for (size_t p = 0; p < instance.properties.size(); p++) {
+		MeshInfo *info = _meshInfo->copyWithoutMesh();
 		std::vector<double> *values = new std::vector<double>();
 		Point point;
 
 		for (size_t d = 0; d < instance.domains; d++) {
 			for (size_t i = 0; i < instance.B1[d].I_row_indices.size() && instance.B1[d].I_row_indices[i] <= instance.block[Instance::CONSTRAINT::DIRICHLET]; i++) {
 				const Element *e = _mesh->getDOFsElement(d, instance.B1[d].J_col_indices[i] - 1);
+				if (e->DOFOffset(d, instance.B1[d].J_col_indices[i] - 1) != p) {
+					continue;
+				}
 				point = _meshInfo->shrink(_mesh->coordinates()[e->node(0)], d);
 				info->_regions[0].coordinates.insert(info->_regions[0].coordinates.end(), { point.x, point.y, point.z });
 				values->push_back(instance.B1c[d][i]);
@@ -273,11 +280,12 @@ void ResultStore::storeDirichlet(const Step &step, const Instance &instance)
 		std::iota(info->_regions[0].elements.begin(), info->_regions[0].elements.end(), 0);
 		std::stringstream ss;
 		ss << instance.properties[p];
-		info->_regions[0].data.pointDataDouble[ss.str()] = std::make_pair(1, values);
-		store("DIRICHLET_" + ss.str(), step, info);
-		info->clearData();
+		info->_regions[0].data.elementDataDouble[ss.str()] = std::make_pair(1, values);
+		std::vector<std::string> files = store("DIRICHLET_" + ss.str(), step, info);
+		_FETIdata.push_back(std::make_pair(step, files));
+		delete info;
 	}
-	delete info;
+
 }
 
 void ResultStore::storeLambdas(const Step &step, const Instance &instance)
@@ -286,146 +294,120 @@ void ResultStore::storeLambdas(const Step &step, const Instance &instance)
 		// it is pointless to store lambdas for collected result
 		return;
 	}
-	MeshInfo *info = _meshInfo->copyWithoutMesh();
 
 	std::vector<int> neighbours(environment->MPIsize);
 	std::iota(neighbours.begin(), neighbours.end(), 0);
 
-	std::vector<std::pair<esglobal, esglobal> > lMap;
+	for (size_t p = 0; p < instance.properties.size(); p++) {
+		MeshInfo *info = _meshInfo->copyWithoutMesh();
 
-	for (size_t i = 0; i < instance.B1clustersMap.size(); i++) {
-		if (instance.B1clustersMap[i].front() < instance.block[Instance::CONSTRAINT::DIRICHLET]) {
-			continue;
+		std::vector<double> duplicity, *values = new std::vector<double>();
+		std::vector<std::pair<eslocal, eslocal> > lambdaMap;
+
+		std::vector<std::vector<esglobal> > sLambdas(environment->MPIsize);
+		std::vector<std::vector<Point> > sPoints(environment->MPIsize);
+		std::vector<std::vector<esglobal> > rLambdas(environment->MPIsize);
+		std::vector<std::vector<Point> > rPoints(environment->MPIsize);
+		Point point;
+
+		for (size_t d = 0; d < instance.domains; d++) {
+			auto start = std::upper_bound(instance.B1[d].I_row_indices.begin(), instance.B1[d].I_row_indices.end(), instance.block[Instance::CONSTRAINT::DIRICHLET]);
+			auto end = std::upper_bound(instance.B1[d].I_row_indices.begin(), instance.B1[d].I_row_indices.end(), instance.block[Instance::CONSTRAINT::DIRICHLET] + instance.block[Instance::CONSTRAINT::EQUALITY_CONSTRAINTS]);
+			for (size_t i = start - instance.B1[d].I_row_indices.begin(); i < end - instance.B1[d].I_row_indices.begin(); i++) {
+				auto it = std::lower_bound(instance.B1clustersMap.begin(), instance.B1clustersMap.end(), instance.B1[d].I_row_indices[i] - 1, [&] (const std::vector<esglobal> &v, esglobal i) {
+					return v[0] < i;
+				});
+				const Element *e = _mesh->getDOFsElement(d, instance.B1[d].J_col_indices[i] - 1);
+				if (e->DOFOffset(d, instance.B1[d].J_col_indices[i] - 1) != p) {
+					continue;
+				}
+
+				point = info->shrink(_mesh->coordinates()[e->node(0)], d);
+				info->_regions[0].coordinates.insert(info->_regions[0].coordinates.end(), { point.x, point.y, point.z, 0, 0, 0 });
+				lambdaMap.push_back(std::make_pair(instance.B1[d].I_row_indices[i], (eslocal)duplicity.size()));
+				values->push_back(instance.B1[d].V_values[i]);
+				duplicity.push_back(instance.B1duplicity[d][i]);
+
+				if (it->size() == 3) {
+					sLambdas[(*it)[2]].push_back(it->front());
+					sPoints[(*it)[2]].push_back(point);
+				}
+			}
 		}
-		if (instance.B1clustersMap[i].size() == 2) {
-			lMap.push_back(std::make_pair((esglobal)instance.B1clustersMap[i][0], (esglobal)(lMap.size())));
-			lMap.push_back(std::make_pair((esglobal)instance.B1clustersMap[i][0], (esglobal)(lMap.size())));
-		} else {
-			lMap.push_back(std::make_pair((esglobal)instance.B1clustersMap[i][0], (esglobal)(lMap.size())));
+
+		std::sort(lambdaMap.begin(), lambdaMap.end());
+
+
+		if (!Communication::exchangeUnknownSize(sLambdas, rLambdas, neighbours)) {
+			ESINFO(ERROR) << "problem while exchanging Lambdas in storeLambdas.";
 		}
-	}
-
-	std::vector<double> duplicity(lMap.size()), *values = new std::vector<double>(lMap.size());
-
-	info->_regions[0].coordinates.resize(6 * lMap.size());
-	info->_regions[0].elementsTypes.resize(lMap.size(), -1);
-	info->_regions[0].elementsNodes.reserve(lMap.size());
-	info->_regions[0].elements.reserve(2 * lMap.size());
-
-	for (size_t i = 0; i < lMap.size(); i++) {
-		info->_regions[0].elementsNodes.push_back(2 * i + 2);
-		info->_regions[0].elements.push_back(2 * i);
-		info->_regions[0].elements.push_back(2 * i + 1);
-	}
-
-	auto lIndex = [&] (esglobal lambda) {
-		std::pair<esglobal, esglobal> pair(lambda, 0);
-		auto it = std::lower_bound(lMap.begin(), lMap.end(), pair);
-		if (it == lMap.end() || it->first != lambda) {
-			ESINFO(ERROR) << "Invalid lamdas in B1.";
+		if (!Communication::exchangeUnknownSize(sPoints, rPoints, neighbours)) {
+			ESINFO(ERROR) << "problem while exchanging Points in storeLambdas.";
 		}
-		return it - lMap.begin();
-	};
+		for (int i = 0; i < environment->MPIsize; i++) {
+			if (sLambdas[i].size() != rLambdas[i].size() || sPoints[i].size() != rPoints[i].size()) {
+				ESINFO(ERROR) << "Lambda indices do not match.";
+			}
+		}
 
-	std::vector<std::vector<esglobal> > sLambdas(environment->MPIsize);
-	std::vector<std::vector<Point> > sPoints(environment->MPIsize);
-	std::vector<std::vector<esglobal> > rLambdas(environment->MPIsize);
-	std::vector<std::vector<Point> > rPoints(environment->MPIsize);
-	Point point;
+		for (size_t d = 0, offset = 0; d < instance.domains; d++) {
+			auto start = std::upper_bound(instance.B1[d].I_row_indices.begin(), instance.B1[d].I_row_indices.end(), instance.block[Instance::CONSTRAINT::DIRICHLET]);
+			auto end = std::upper_bound(instance.B1[d].I_row_indices.begin(), instance.B1[d].I_row_indices.end(), instance.block[Instance::CONSTRAINT::DIRICHLET] + instance.block[Instance::CONSTRAINT::EQUALITY_CONSTRAINTS]);
+			for (size_t i = start - instance.B1[d].I_row_indices.begin(); i < end - instance.B1[d].I_row_indices.begin(); i++) {
+				auto it = std::lower_bound(instance.B1clustersMap.begin(), instance.B1clustersMap.end(), instance.B1[d].I_row_indices[i] - 1, [&] (const std::vector<esglobal> &v, esglobal i) {
+					return v[0] < i;
+				});
+				const Element *e = _mesh->getDOFsElement(d, instance.B1[d].J_col_indices[i] - 1);
+				if (e->DOFOffset(d, instance.B1[d].J_col_indices[i] - 1) != p) {
+					continue;
+				}
 
-	for (size_t d = 0; d < instance.domains; d++) {
-		auto start = std::upper_bound(instance.B1[d].I_row_indices.begin(), instance.B1[d].I_row_indices.end(), instance.block[Instance::CONSTRAINT::DIRICHLET]);
-		auto end = std::upper_bound(instance.B1[d].I_row_indices.begin(), instance.B1[d].I_row_indices.end(), instance.block[Instance::CONSTRAINT::DIRICHLET] + instance.block[Instance::CONSTRAINT::EQUALITY_CONSTRAINTS]);
-		for (size_t i = start - instance.B1[d].I_row_indices.begin(); i < end - instance.B1[d].I_row_indices.begin(); i++) {
-			auto it = std::lower_bound(instance.B1clustersMap.begin(), instance.B1clustersMap.end(), instance.B1[d].I_row_indices[i] - 1, [&] (const std::vector<esglobal> &v, esglobal i) {
-				return v[0] < i;
-			});
-			const Element *e = _mesh->getDOFsElement(d, instance.B1[d].J_col_indices[i] - 1);
-			point = info->shrink(_mesh->coordinates()[e->node(0)], d);
-			size_t index = lIndex(it->front());
-			for (size_t c = 1; c < it->size(); c++) {
-				if ((*it)[c] == environment->MPIrank) {
-					if (info->_regions[0].elementsTypes[index] != -1) {
-						index++;
-						if (lMap[index].first != it->front()) {
-							ESINFO(ERROR) << "Broken B1.";
-						}
+				Point a(info->_regions[0].coordinates[6 * offset + 0], info->_regions[0].coordinates[6 * offset + 1], info->_regions[0].coordinates[6 * offset + 2]);
+				Point b;
+				if (it->size() == 3) {
+					size_t index = std::find(rLambdas[(*it)[2]].begin(), rLambdas[(*it)[2]].end(), it->front()) - rLambdas[(*it)[2]].begin();
+					if (index == rLambdas[(*it)[2]].size() || rLambdas[(*it)[2]][index] != it->front()) {
+						ESINFO(ERROR) << "Different Lambdas on neighbour clusters.";
 					}
-					info->_regions[0].elementsTypes[index] = Line2VTKCode;
-					info->_regions[0].coordinates[6 * index + 0] = point.x;
-					info->_regions[0].coordinates[6 * index + 1] = point.y;
-					info->_regions[0].coordinates[6 * index + 2] = point.z;
-					(*values)[index] = instance.B1[d].V_values[i];
-					duplicity[index] = instance.B1duplicity[d][i];
+					b = rPoints[(*it)[2]][index];
 				} else {
-					sLambdas[(*it)[c]].push_back(it->front());
-					sPoints[(*it)[c]].push_back(point);
+					auto lit = std::lower_bound(lambdaMap.begin(), lambdaMap.end(), std::make_pair(instance.B1[d].I_row_indices[i], -1));
+					if (lit == lambdaMap.end()) {
+						ESINFO(ERROR) << "Incorrect B1 lambdas.";
+					}
+					if (lit->second == (eslocal)offset) {
+						lit++;
+					}
+					b.x = info->_regions[0].coordinates[6 * lit->second + 0];
+					b.y = info->_regions[0].coordinates[6 * lit->second + 1];
+					b.z = info->_regions[0].coordinates[6 * lit->second + 2];
 				}
+				Point length = b - a;
+				Point ax = a + length * duplicity[offset];
+				info->_regions[0].coordinates[6 * offset + 3] = ax.x;
+				info->_regions[0].coordinates[6 * offset + 4] = ax.y;
+				info->_regions[0].coordinates[6 * offset + 5] = ax.z;
+				offset++;
 			}
 		}
-	}
 
 
-	if (!Communication::exchangeUnknownSize(sLambdas, rLambdas, neighbours)) {
-		ESINFO(ERROR) << "problem while exchanging Lambdas in storeLambdas.";
-	}
-	if (!Communication::exchangeUnknownSize(sPoints, rPoints, neighbours)) {
-		ESINFO(ERROR) << "problem while exchanging Points in storeLambdas.";
-	}
-	for (int i = 0; i < environment->MPIsize; i++) {
-		if (sLambdas[i].size() != rLambdas[i].size() || sPoints[i].size() != rPoints[i].size()) {
-			ESINFO(ERROR) << "Lambda indices do not match.";
+		size_t size = info->_regions[0].coordinates.size() / 6;
+		info->_regions[0].elementsTypes.resize(size, Line2VTKCode);
+		info->_regions[0].elementsNodes.reserve(size);
+		info->_regions[0].elements.reserve(2 * size);
+
+		for (size_t i = 0; i < size; i++) {
+			info->_regions[0].elementsNodes.push_back(2 * i + 2);
+			info->_regions[0].elements.push_back(2 * i);
+			info->_regions[0].elements.push_back(2 * i + 1);
 		}
-	}
 
-	for (size_t d = 0; d < instance.domains; d++) {
-		auto start = std::upper_bound(instance.B1[d].I_row_indices.begin(), instance.B1[d].I_row_indices.end(), instance.block[Instance::CONSTRAINT::DIRICHLET]);
-		auto end = std::upper_bound(instance.B1[d].I_row_indices.begin(), instance.B1[d].I_row_indices.end(), instance.block[Instance::CONSTRAINT::DIRICHLET] + instance.block[Instance::CONSTRAINT::EQUALITY_CONSTRAINTS]);
-		for (size_t i = start - instance.B1[d].I_row_indices.begin(); i < end - instance.B1[d].I_row_indices.begin(); i++) {
-			auto it = std::lower_bound(instance.B1clustersMap.begin(), instance.B1clustersMap.end(), instance.B1[d].I_row_indices[i] - 1, [&] (const std::vector<esglobal> &v, esglobal i) {
-				return v[0] < i;
-			});
-			size_t lindex = lIndex(it->front());
-			for (size_t c = 2; c < it->size(); c++) {
-				size_t index = std::find(rLambdas[(*it)[c]].begin(), rLambdas[(*it)[c]].end(), it->front()) - rLambdas[(*it)[c]].begin();
-				if (index == rLambdas[(*it)[c]].size() || rLambdas[(*it)[c]][index] != it->front()) {
-					ESINFO(ERROR) << "Different Lambdas on neighbour clusters.";
-				}
-				info->_regions[0].coordinates[6 * lindex + 3] = rPoints[(*it)[c]][index].x;
-				info->_regions[0].coordinates[6 * lindex + 4] = rPoints[(*it)[c]][index].y;
-				info->_regions[0].coordinates[6 * lindex + 5] = rPoints[(*it)[c]][index].z;
-			}
-		}
+		info->_regions[0].data.elementDataDouble["B1values"] = std::make_pair(1, values);
+		std::vector<std::string> files = store("EQUALITY_CONSTRAINTS", step, info);
+		_FETIdata.push_back(std::make_pair(step, files));
+		delete info;
 	}
-
-	for (size_t i = 0; i < lMap.size(); i++) {
-		if (i + 1 < lMap.size() && lMap[i].first == lMap[i + 1].first) {
-			Point a(info->_regions[0].coordinates[6 * (i + 0) + 0], info->_regions[0].coordinates[6 * (i + 0) + 1], info->_regions[0].coordinates[6 * (i + 0) + 2]);
-			Point b(info->_regions[0].coordinates[6 * (i + 1) + 0], info->_regions[0].coordinates[6 * (i + 1) + 1], info->_regions[0].coordinates[6 * (i + 1) + 2]);
-			Point length = b - a;
-			Point ax = a + length * duplicity[i];
-			Point bx = b - length * duplicity[i + 1];
-			info->_regions[0].coordinates[6 * (i + 0) + 3] = ax.x;
-			info->_regions[0].coordinates[6 * (i + 0) + 4] = ax.y;
-			info->_regions[0].coordinates[6 * (i + 0) + 5] = ax.z;
-			info->_regions[0].coordinates[6 * (i + 1) + 3] = bx.x;
-			info->_regions[0].coordinates[6 * (i + 1) + 4] = bx.y;
-			info->_regions[0].coordinates[6 * (i + 1) + 5] = bx.z;
-			i++;
-		} else {
-			Point a(info->_regions[0].coordinates[6 * (i + 0) + 0], info->_regions[0].coordinates[6 * (i + 0) + 1], info->_regions[0].coordinates[6 * (i + 0) + 2]);
-			Point b(info->_regions[0].coordinates[6 * (i + 0) + 3], info->_regions[0].coordinates[6 * (i + 0) + 4], info->_regions[0].coordinates[6 * (i + 0) + 5]);
-			Point length = b - a;
-			Point ax = a + length * duplicity[i];
-			info->_regions[0].coordinates[6 * (i + 0) + 3] = ax.x;
-			info->_regions[0].coordinates[6 * (i + 0) + 4] = ax.y;
-			info->_regions[0].coordinates[6 * (i + 0) + 5] = ax.z;
-		}
-	}
-
-	info->_regions[0].data.elementDataDouble["values"] = std::make_pair(1, values);
-	store("EQUALITY_CONSTRAINTS", step, info);
-	delete info;
 }
 
 
