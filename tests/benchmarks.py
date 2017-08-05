f9f502a210bc3feb1fb0e8ee2b6b2417b5aa4515
Ondrej Meca <ondrej.meca@gmail.com>
2017-08-05 14:57:51 +0200

    FIX: set benchmarks config file

--- tests/benchmarks.py
+++ tests/benchmarks.py
@@ -18,7 +18,7 @@ class ESPRESOBenchmarks(unittest.TestCase):
         self.espreso.run(
             self.espreso.get_processes(os.path.join(path, file)),
             path,
-            { "c": file, "ENV::TESTING_LEVEL": 0, "ENV::VERBOSE_LEVEL": 0, "ENV::MEASURE_LEVEL": 0, "OUTPUT::RESULTS": 0 }
+            { "config": file, "ENV::TESTING_LEVEL": 0, "ENV::VERBOSE_LEVEL": 0, "ENV::MEASURE_LEVEL": 0, "OUTPUT::RESULTS": 0 }
         )
 
         self.espreso.compare_monitors(
