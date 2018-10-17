ba3913684dc6cb6d5883fb11ef43e29f10e540e3
pan0068 <tomas.panoc@vsb.cz>
2018-10-17 10:51:43 +0200

    FIX: ECFConstraint copy and move constructors, missing prefix attribute

--- src/config/ecf/material/material.cpp
+++ src/config/ecf/material/material.cpp
@@ -2,6 +2,7 @@
 #include "material.h"
 
 #include "config/configuration.hpp"
+#include "config/conditions.h"
 
 using namespace espreso;
 
@@ -15,47 +16,58 @@ MaterialBaseConfiguration::MaterialBaseConfiguration(DIMENSION *D, PHYSICAL_MODE
 {
 	REGISTER(coordinate_system, ECFMetaData()
 			.setdescription({ "Coordinate system" })
-			.allowonly([&] () { return !*_phase_change; }));
+			.setcollapsed()
+			.allowonly([&] () { return !*_phase_change; })
+			.addconstraint(ECFCondition(*_phase_change, ECFCondition::EQUALS, false)));
 
 	density.value = heat_capacity.value = "0";
 	ecfdescription->registerParameter("dens", density, ECFMetaData()
 			.setdescription({ "Density" })
 			.setdatatype({ ECFDataType::EXPRESSION })
-			.setunit(SIUnit(-3, 1, 0, 0, 0, 0, 0))
-			.allowonly([&] () { return !*_phase_change; }));
+			.allowonly([&] () { return !*_phase_change; })
+			.addconstraint(ECFCondition(*_phase_change, ECFCondition::EQUALS, false)));
 
 	ecfdescription->registerParameter("CP", heat_capacity, ECFMetaData()
 			.setname("Heat capacity")
 			.setdescription({ "Heat capacity" })
 			.setdatatype({ ECFDataType::EXPRESSION })
-			.allowonly([&] () { return !*_phase_change; }));
+			.setunit(Unit().add(Unit::UnitLibrary::KILOGRAM, 1)
+						.add(Unit::UnitLibrary::METRE, 2)
+						.add(Unit::UnitLibrary::SECOND, -2)
+						.add(Unit::UnitLibrary::KELVIN, -1))
+			.allowonly([&] () { return !*_phase_change; })
+			.addconstraint(ECFCondition(*_phase_change, ECFCondition::EQUALS, false)));
 
 	if (physical_model & PHYSICAL_MODEL::STRUCTURAL_MECHANICS) {
 		REGISTER(linear_elastic_properties, ECFMetaData()
 				.setdescription({ "Linear elasticity" })
 				.allowonly([&] () { return
-						(!*_phase_change) &&
+						(_phase_change == NULL || !*_phase_change) &&
 						(physical_model & PHYSICAL_MODEL::STRUCTURAL_MECHANICS) &&
-						(material_model == MATERIAL_MODEL::LINEAR_ELASTIC); }));
+						(material_model == MATERIAL_MODEL::LINEAR_ELASTIC); })
+				.addconstraint(ECFCondition(*_phase_change, ECFCondition::EQUALS, false) & ECFCondition(material_model, ECFCondition::EQUALS, MATERIAL_MODEL::LINEAR_ELASTIC)));
 
 		REGISTER(hyper_elastic_properties, ECFMetaData()
 				.setdescription({ "Hyper elasticity" })
 				.allowonly([&] () { return
-						(!*_phase_change) &&
+						(_phase_change == NULL || !*_phase_change) &&
 						(physical_model & PHYSICAL_MODEL::STRUCTURAL_MECHANICS) &&
-						(material_model == MATERIAL_MODEL::HYPER_ELASTIC); }));
+						(material_model == MATERIAL_MODEL::HYPER_ELASTIC); })
+				.addconstraint(ECFCondition(*_phase_change, ECFCondition::EQUALS, false) & ECFCondition(material_model, ECFCondition::EQUALS, MATERIAL_MODEL::HYPER_ELASTIC)));
+		
 		REGISTER(thermal_expansion, ECFMetaData()
 				.setdescription({ "Thermal expansion" })
 				.allowonly([&] () {
-					return (!*_phase_change) && (physical_model & PHYSICAL_MODEL::THERMAL);
+					return (_phase_change == NULL || !*_phase_change) && (physical_model & PHYSICAL_MODEL::THERMAL);
 				}));
 	}
 
 	if (physical_model & PHYSICAL_MODEL::THERMAL) {
 		REGISTER(thermal_conductivity, ECFMetaData()
 				.setdescription({ "Thermal conductivity" })
+				.noexport()
 				.allowonly([&] () {
-					return (!*_phase_change) && (physical_model & PHYSICAL_MODEL::THERMAL);
+					return (_phase_change == NULL || !*_phase_change) && (physical_model & PHYSICAL_MODEL::THERMAL);
 				}));
 	}
 }
@@ -64,16 +76,6 @@ MaterialConfiguration::MaterialConfiguration(DIMENSION *D, PHYSICAL_MODEL physic
 : MaterialBaseConfiguration(D, physicalModel, &phase_change),
   phase_change(false)
 {
-	REGISTER(dimension, ECFMetaData()
-				.setdescription({"Dimension"})
-				.setdatatype({ ECFDataType::OPTION })
-				.addoption(ECFOption().setname("D1").setdescription("D1"))
-				.addoption(ECFOption().setname("D2").setdescription("D2"))
-				.addoption(ECFOption().setname("D3").setdescription("D3"))
-				.addoption(ECFOption().setname("Z").setdescription("Z"))
-				.allowonly([&] () { return false; })
-				.addconstraint(ECFFalseCondition()));
-
 	name = "";
 	REGISTER(name, ECFMetaData()
 			 .setdescription({ "Name" })
@@ -84,24 +86,27 @@ MaterialConfiguration::MaterialConfiguration(DIMENSION *D, PHYSICAL_MODEL physic
 			 .setdescription({ "Description" })
 			 .setdatatype( { ECFDataType::STRING } ));
 
-	ecfdescription->addSpace();
+	ecfdescription->addSpace()->metadata.noexport();
 
 	REGISTER(physical_model, ECFMetaData()
 			.setdescription({ "Physical model" })
 			.setdatatype({ ECFDataType::ENUM_FLAGS })
+			.noexport()
 			.addoption(ECFOption().setname("THERMAL").setdescription("Model used by HEAT TRANSFER.")
 					.allowonly([&] () { return physicalModel & PHYSICAL_MODEL::THERMAL; }))
 			.addoption(ECFOption().setname("STRUCTURAL_MECHANICS").setdescription("One of models used by STRUCTURAL MECHANICS.")
 					.allowonly([&] () { return physicalModel & PHYSICAL_MODEL::STRUCTURAL_MECHANICS; })));
-
-	REGISTER(material_model, ECFMetaData()
-			.setdescription({ "Material model" })
-			.setdatatype({ ECFDataType::OPTION })
-			.addoption(ECFOption().setname("LINEAR_ELASTIC").setdescription("Linear elastic model.")
-					.allowonly([&] () { return physicalModel & PHYSICAL_MODEL::STRUCTURAL_MECHANICS; }))
-			.addoption(ECFOption().setname("HYPER_ELASTIC").setdescription("Hyper elastic model.")
-					.allowonly([&] () { return physicalModel & PHYSICAL_MODEL::STRUCTURAL_MECHANICS; })));
-
+	
+	if (physicalModel & PHYSICAL_MODEL::STRUCTURAL_MECHANICS)
+	{
+		REGISTER(material_model, ECFMetaData()
+				.setdescription({ "Material model" })
+				.setdatatype({ ECFDataType::OPTION })
+				.addoption(ECFOption().setname("LINEAR_ELASTIC").setdescription("Linear elastic model.")
+						.allowonly([&] () { return physicalModel & PHYSICAL_MODEL::STRUCTURAL_MECHANICS; }))
+				.addoption(ECFOption().setname("HYPER_ELASTIC").setdescription("Hyper elastic model.")
+						.allowonly([&] () { return physicalModel & PHYSICAL_MODEL::STRUCTURAL_MECHANICS; })));
+	}
 	ecfdescription->addSeparator();
 
 	ecfdescription->moveLastBefore(ecfdescription->parameters.front()->name);
@@ -109,7 +114,7 @@ MaterialConfiguration::MaterialConfiguration(DIMENSION *D, PHYSICAL_MODEL physic
 	ecfdescription->moveLastBefore(ecfdescription->parameters.front()->name);
 	ecfdescription->moveLastBefore(ecfdescription->parameters.front()->name);
 	ecfdescription->moveLastBefore(ecfdescription->parameters.front()->name);
-	ecfdescription->moveLastBefore(ecfdescription->parameters.front()->name);
+	// ecfdescription->moveLastBefore(ecfdescription->parameters.front()->name);
 
 	REGISTER(phase_change, ECFMetaData()
 	.setdescription({ "Phase change" })
@@ -119,29 +124,56 @@ MaterialConfiguration::MaterialConfiguration(DIMENSION *D, PHYSICAL_MODEL physic
 	REGISTER(smooth_step_order, ECFMetaData()
 			.setdescription({ "Smooth step order" })
 			.setdatatype({ ECFDataType::NONNEGATIVE_INTEGER })
-			.allowonly([&] () { return phase_change; }));
+			.setrange(0, 100)
+			.allowonly([&] () { return phase_change; })
+			.addconstraint(ECFCondition(phase_change, ECFCondition::EQUALS, true)));
 
 	latent_heat = transition_interval = phase_change_temperature = 0;
 	REGISTER(latent_heat, ECFMetaData()
 			.setdescription({ "Latent heat" })
 			.setdatatype({ ECFDataType::FLOAT })
-			.allowonly([&] () { return phase_change; }));
+			.allowonly([&] () { return phase_change; })
+			.addconstraint(ECFCondition(phase_change, ECFCondition::EQUALS, true)));
 	REGISTER(transition_interval, ECFMetaData()
 				.setdescription({ "Transition interval" })
 				.setdatatype({ ECFDataType::FLOAT })
-				.allowonly([&] () { return phase_change; }));
+				.allowonly([&] () { return phase_change; })
+				.addconstraint(ECFCondition(phase_change, ECFCondition::EQUALS, true)));
 	REGISTER(phase_change_temperature, ECFMetaData()
 				.setdescription({ "Phase change temperature" })
 				.setdatatype({ ECFDataType::FLOAT })
-				.allowonly([&] () { return phase_change; }));
+				.allowonly([&] () { return phase_change; })
+				.addconstraint(ECFCondition(phase_change, ECFCondition::EQUALS, true)));
 
 	REGISTER(phases, ECFMetaData()
 			.setdescription({ "Phase change settings.", "Phase settings" })
 			.setdatatype({ ECFDataType::POSITIVE_INTEGER })
 			.setpattern({ "1" })
-			.allowonly([&] () { return phase_change; }),
+			.setgroup()
+			.allowonly([&] () { return phase_change; })
+			.addconstraint(ECFCondition(phase_change, ECFCondition::EQUALS, true)),
 			D, physical_model, &phase_change);
 
-	ecfdescription->getParameter(&phases)->getParameter("1");
-	ecfdescription->getParameter(&phases)->getParameter("2");
+	auto removePhaseConstraints = [&] (ECFParameter* object) {
+		ECFObject* obj = static_cast<ECFObject*>(object);
+		obj->forEachParameters([&] (ECFParameter* p){
+			if (p->metadata.condition->isset())
+			{
+				if (p->metadata.condition->match(&phase_change))
+				{
+					p->metadata.removeconstraint();
+				}
+			}
+		}, true, false, true);
+	};
+	ECFParameter* ph = ecfdescription->getParameter(&phases)->getParameter("1");
+	ph->metadata.name = "Phase 1";
+	ph->metadata.removeconstraint();
+	ph->metadata.setcollapsed();
+	removePhaseConstraints(ph);
+	ph = ecfdescription->getParameter(&phases)->getParameter("2");
+	ph->metadata.name = "Phase 2";
+	ph->metadata.removeconstraint();
+	ph->metadata.setcollapsed();
+	removePhaseConstraints(ph);
 }
