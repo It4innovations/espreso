6b02bfe9d149ba7955886b571e748bee79fbb3fe
Ondrej Meca <ondrej.meca@gmail.com>
2020-11-16 13:26:00 +0100

    TEST: update morphing tests

--- benchmarks/diffusion3D/steadystate/morphing/__init__.py
+++ benchmarks/diffusion3D/steadystate/morphing/__init__.py
@@ -2,7 +2,7 @@
 from estest import ESPRESOTest
 
 def setup():
-    ESPRESOTest.processes = 4
-    ESPRESOTest.env["OMP_NUM_THREADS"] = "4"
-    ESPRESOTest.env["SOLVER_NUM_THREADS"] = "4"
-    ESPRESOTest.env["PAR_NUM_THREADS"] = "4"
+    ESPRESOTest.processes = 1
+    ESPRESOTest.env["OMP_NUM_THREADS"] = "2"
+    ESPRESOTest.env["SOLVER_NUM_THREADS"] = "2"
+    ESPRESOTest.env["PAR_NUM_THREADS"] = "2"
---
+++ benchmarks/diffusion3D/steadystate/morphing/hat/tested.py
@@ -0,0 +1,23 @@
+
+import os
+from nose.tools import istest
+
+from estest import ESPRESOTest
+
+def setup():
+    ESPRESOTest.path = os.path.dirname(__file__)
+    ESPRESOTest.args = [ "etype", 1, 1, 1, 3, 2, 4, 3, 5, 2 ]
+
+def teardown():
+    ESPRESOTest.clean()
+
+@istest
+def by():
+    for etype in [ "HEXA8", "HEXA20", "TETRA4", "TETRA10", "PRISMA6", "PRISMA15", "PYRAMID5", "PYRAMID13" ]:
+        yield run, etype
+        if ESPRESOTest.fast: return
+
+def run(etype):
+    ESPRESOTest.args[0] = etype
+    ESPRESOTest.run()
+    ESPRESOTest.compare_emr(".".join([etype, "emr"]))
