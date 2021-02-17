9198296aa941b33e96875107f6dc6a1c51ac5b01
Ondrej Meca <ondrej.meca@gmail.com>
2021-02-17 07:46:13 +0100

    TEST: add multiple-contact test

---
+++ benchmarks/contact/threecubes/tested.py
@@ -0,0 +1,21 @@
+
+import os
+from nose.tools import istest
+
+from estest import ESPRESOTest
+
+def setup():
+    ESPRESOTest.path = os.path.dirname(__file__)
+
+def teardown():
+    ESPRESOTest.clean()
+
+@istest
+def by():
+    for p in range(1, 8):
+        yield run, p
+
+def run(p):
+    ESPRESOTest.processes = p
+    ESPRESOTest.compare_mesh("espreso.log", ESPRESOTest.run())
+    ESPRESOTest.compare_emr("espreso.emr")
