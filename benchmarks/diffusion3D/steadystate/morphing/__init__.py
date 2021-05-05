cc08f4c7fe026b47dbb143a98d3615f0b87ef330
Michal Kravcenko <kra568@vsb.cz>
2021-05-05 13:51:34 +0200

    [FIX] updated benchmarks to reflect changes in ACA code and configuration

--- benchmarks/diffusion3D/steadystate/morphing/__init__.py
+++ benchmarks/diffusion3D/steadystate/morphing/__init__.py
@@ -3,6 +3,6 @@ from estest import ESPRESOTest
 
 def setup():
     ESPRESOTest.processes = 1
-    ESPRESOTest.env["OMP_NUM_THREADS"] = "2"
-    ESPRESOTest.env["SOLVER_NUM_THREADS"] = "2"
-    ESPRESOTest.env["PAR_NUM_THREADS"] = "2"
+    ESPRESOTest.env["OMP_NUM_THREADS"] = "1"
+    ESPRESOTest.env["SOLVER_NUM_THREADS"] = "1"
+    ESPRESOTest.env["PAR_NUM_THREADS"] = "1"
--- benchmarks/diffusion3D/steadystate/morphing/bent_disk/tested.py
+++ benchmarks/diffusion3D/steadystate/morphing/bent_disk/tested.py
@@ -6,7 +6,7 @@ from estest import ESPRESOTest
 
 def setup():
     ESPRESOTest.path = os.path.dirname(__file__)
-    ESPRESOTest.args = [ "etype", 1, 1, 1, 3, 2, 4, 16, 16, 32 ]
+    ESPRESOTest.args = [ "etype", 1, 1, 1, 3, 4, 2, 16, 16, 16 ]
 
 def teardown():
     ESPRESOTest.clean()
@@ -14,13 +14,19 @@ def teardown():
 @istest
 def by():
     for etype in [ "HEXA8" ]:
-        for p in range(1, 32):
-            yield run, p, etype
+        for p in [[1, 1, 1], [2, 1, 1], [2, 2, 1], [2, 2, 2], [4, 2, 2], [4, 4, 2]]:
+            dim = 16;
+            yield run, p[0], p[1], p[2], dim, etype
             if ESPRESOTest.fast: return
 
-def run(p, etype):
-    ESPRESOTest.processes = p
+def run(p1, p2, p3, dim, etype):
+    ESPRESOTest.processes = p1*p2*p3
     ESPRESOTest.args[0] = etype
-    ESPRESOTest.args[3] = p
+    ESPRESOTest.args[1] = p1
+    ESPRESOTest.args[2] = p2
+    ESPRESOTest.args[3] = p3
+    ESPRESOTest.args[7] = dim/p1
+    ESPRESOTest.args[8] = dim/p2
+    ESPRESOTest.args[9] = dim/p3
     ESPRESOTest.run()
     ESPRESOTest.compare_emr(".".join([etype, "emr"]))
