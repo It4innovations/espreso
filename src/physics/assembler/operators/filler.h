504d87785e1debe1f8d84cbcc1870c7eded54b43
xkadlu01 <xkadlu01@login1.barbora.it4i.cz>
2021-06-03 14:45:32 +0200

    ENH: SIMD matrix filler

--- src/physics/assembler/operators/filler.h
+++ src/physics/assembler/operators/filler.h
@@ -131,6 +131,96 @@ struct RHSFiller: public BoundaryOperatorBuilder {
 	}
 };
 
+struct KFillerSimd: public Operator {
+	KFillerSimd(const ParameterData &stiffness, double *K, esint *perm, PerElementSize size, int interval)
+	: Operator(interval, false, true),
+	  stiffness(stiffness, interval),
+	  K(K), perm(perm),
+	  size(stiffness.increment(size, interval)),
+	  elementInPack(0) {}
+
+	InputParameterIterator stiffness;
+	double *K;
+	esint *perm;
+	int size;
+	int elementInPack;
+
+	void operator++()
+	{
+		// Loop iterate_elements_simd cant be used -- overfill of K when elements not multiple of SIMD lenght
+		// Compicated operator++ will move iterator by SIMD elements when processed
+		// indexing within SIMD pack done manually. (see derived operators). Workaround for now
+		elementInPack++;
+		if(elementInPack == SIMD::size)
+		{
+			stiffness += SIMD::size;
+			elementInPack = 0;	
+		}
+	}
+};
+
+struct KSymmFillerSimd: public KFillerSimd {
+	using KFillerSimd::KFillerSimd;
+
+	void operator()()
+	{
+		for (int r = 0; r < size; ++r) {
+			for (int c = 0; c < size; ++c) {
+				if (r <= c) {
+					int item = r * size + c;
+					K[*perm++] += stiffness.data[item * SIMD::size + elementInPack];
+				}
+			}
+		}
+	}
+};
+
+struct KFullFillerSimd: public KFillerSimd {
+	using KFillerSimd::KFillerSimd;
+
+	void operator()()
+	{
+		for (int r = 0; r < size; ++r) {
+			for (int c = 0; c < size; ++c) {
+				int item = r * size + c;
+				K[*perm++] += stiffness.data[item * SIMD::size + elementInPack];
+			}
+		}
+	}
+};
+
+struct MatricesFillerSimd: public ElementOperatorBuilder {
+	HeatTransferModuleOpt &kernel;
+	bool issymmetric;
+	double *K;
+	esint *perm;
+
+	MatricesFillerSimd(HeatTransferModuleOpt &kernel, double *K, esint *perm): ElementOperatorBuilder("FILL K"), kernel(kernel), issymmetric(true), K(K), perm(perm)
+	{
+		// TODO: allow various symmetricity for each interval
+		for (size_t i = 0; i < kernel.translationMotions.gp.isset.size(); ++i) {
+			issymmetric &= !kernel.translationMotions.gp.isset[i];
+		}
+		for (size_t i = 0; i < kernel.material.model.anisotropic.isset.size(); ++i) {
+			issymmetric &= !kernel.material.model.anisotropic.isset[i];
+		}
+	}
+
+	bool build(HeatTransferModuleOpt &kernel) override
+	{
+		return true;
+	}
+
+	void apply(int interval)
+	{
+		if (issymmetric) {
+			iterate_elements(KSymmFillerSimd(kernel.elementsSimd.stiffness, K, perm, enodes, interval));
+		} else {
+			iterate_elements(KFullFillerSimd(kernel.elementsSimd.stiffness, K, perm, enodes, interval));
+		}
+	}
+};
+
 }
 
 
