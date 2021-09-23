e9ac382b5ea9e3a405799f81753d329e0e07344b
Ondrej Meca <ondrej.meca@vsb.cz>
2021-09-23 14:33:43 +0200

    FIX: compute gradient / flux only when requested

--- src/analysis/assembler/operators/gradient.cpp
+++ src/analysis/assembler/operators/gradient.cpp
@@ -8,12 +8,14 @@ namespace espreso {
 
 void outputGradient(AX_HeatTransfer &module)
 {
-	for(size_t interval = 0; interval < info::mesh->elements->eintervals.size(); ++interval) {
-		if (info::mesh->dimension == 2) {
-			module.elementRes[interval].emplace_back(instantiate<AX_HeatTransfer::NGP, OutputGradient2D>(interval, module.controller, module.integration.dND, module.temp.node, module.gradient.output));
-		}
-		if (info::mesh->dimension == 3) {
-			module.elementRes[interval].emplace_back(instantiate<AX_HeatTransfer::NGP, OutputGradient3D>(interval, module.controller, module.integration.dND, module.temp.node, module.gradient.output));
+	if (module.gradient.output) {
+		for(size_t interval = 0; interval < info::mesh->elements->eintervals.size(); ++interval) {
+			if (info::mesh->dimension == 2) {
+				module.elementRes[interval].emplace_back(instantiate<AX_HeatTransfer::NGP, OutputGradient2D>(interval, module.controller, module.integration.dND, module.temp.node, module.gradient.output));
+			}
+			if (info::mesh->dimension == 3) {
+				module.elementRes[interval].emplace_back(instantiate<AX_HeatTransfer::NGP, OutputGradient3D>(interval, module.controller, module.integration.dND, module.temp.node, module.gradient.output));
+			}
 		}
 	}
 }
