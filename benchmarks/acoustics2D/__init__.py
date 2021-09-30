82e476d0b9e055c609b65008a97f37bbc3bd87ac
Martin Mikšovič <martin.miksovic@vsb.cz>
2021-09-30 16:36:10 +0200

    ENH: add benchmarks for acoustics

---
+++ benchmarks/acoustics2D/__init__.py
@@ -0,0 +1,8 @@
+
+from estest import ESPRESOTest
+
+def setup():
+    ESPRESOTest.processes = 4
+    ESPRESOTest.env["OMP_NUM_THREADS"] = "1"
+    ESPRESOTest.env["SOLVER_NUM_THREADS"] = "1"
+    ESPRESOTest.env["PAR_NUM_THREADS"] = "1"
