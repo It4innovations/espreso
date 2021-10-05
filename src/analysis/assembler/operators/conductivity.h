d32c68464b39b9eada29362468ebc955b3ad873d
Ondrej Meca <ondrej.meca@gmail.com>
2021-10-05 21:45:59 +0200

    RF: remove this pointer

--- src/analysis/assembler/operators/conductivity.h
+++ src/analysis/assembler/operators/conductivity.h
@@ -40,8 +40,8 @@ struct CopyDiagonal2DConductivity: public CopyConductivity {
 		for (size_t gpindex = 0; gpindex < gps; ++gpindex) {
 			int igp = 2 * gpindex;
 			int ogp = 4 * gpindex;
-			this->output.data[ogp + 0] = this->input.data[igp + 0]; this->output.data[ogp + 1] = 0;
-			this->output.data[ogp + 2] = 0;                         this->output.data[ogp + 3] = this->input.data[igp + 1];
+			output.data[ogp + 0] = input.data[igp + 0]; output.data[ogp + 1] = 0;
+			output.data[ogp + 2] = 0;                   output.data[ogp + 3] = input.data[igp + 1];
 		}
 	}
 };
@@ -55,9 +55,9 @@ struct CopyDiagonal3DConductivity: public CopyConductivity {
 		for (size_t gpindex = 0; gpindex < gps; ++gpindex) {
 			int igp = 3 * gpindex;
 			int ogp = 9 * gpindex;
-			this->output.data[ogp + 0] = this->input.data[igp + 0]; this->output.data[ogp + 1] = 0;                         this->output.data[ogp + 2] = 0;
-			this->output.data[ogp + 3] = 0;                         this->output.data[ogp + 4] = this->input.data[igp + 1]; this->output.data[ogp + 5] = 0;
-			this->output.data[ogp + 6] = 0;                         this->output.data[ogp + 7] = 0;                         this->output.data[ogp + 8] = this->input.data[igp + 2];
+			output.data[ogp + 0] = input.data[igp + 0]; output.data[ogp + 1] = 0;                   output.data[ogp + 2] = 0;
+			output.data[ogp + 3] = 0;                   output.data[ogp + 4] = input.data[igp + 1]; output.data[ogp + 5] = 0;
+			output.data[ogp + 6] = 0;                   output.data[ogp + 7] = 0;                   output.data[ogp + 8] = input.data[igp + 2];
 		}
 	}
 };
@@ -71,8 +71,8 @@ struct CopySymmetric2DConductivity: public CopyConductivity {
 		for (size_t gpindex = 0; gpindex < gps; ++gpindex) {
 			int igp = 3 * gpindex;
 			int ogp = 4 * gpindex;
-			this->output.data[ogp + 0] = this->input.data[igp + 0]; this->output.data[ogp + 1] = this->input.data[igp + 2];
-			this->output.data[ogp + 2] = this->input.data[igp + 2]; this->output.data[ogp + 3] = this->input.data[igp + 1];
+			output.data[ogp + 0] = input.data[igp + 0]; output.data[ogp + 1] = input.data[igp + 2];
+			output.data[ogp + 2] = input.data[igp + 2]; output.data[ogp + 3] = input.data[igp + 1];
 		}
 	}
 };
@@ -86,9 +86,9 @@ struct CopySymmetric3DConductivity: public CopyConductivity {
 		for (size_t gpindex = 0; gpindex < gps; ++gpindex) {
 			int igp = 6 * gpindex;
 			int ogp = 9 * gpindex;
-			this->output.data[ogp + 0] = this->input.data[igp + 0]; this->output.data[ogp + 1] = this->input.data[igp + 3]; this->output.data[ogp + 2] = this->input.data[igp + 5];
-			this->output.data[ogp + 3] = this->input.data[igp + 3]; this->output.data[ogp + 4] = this->input.data[igp + 1]; this->output.data[ogp + 5] = this->input.data[igp + 4];
-			this->output.data[ogp + 6] = this->input.data[igp + 5]; this->output.data[ogp + 7] = this->input.data[igp + 4]; this->output.data[ogp + 8] = this->input.data[igp + 2];
+			output.data[ogp + 0] = input.data[igp + 0]; output.data[ogp + 1] = input.data[igp + 3]; output.data[ogp + 2] = input.data[igp + 5];
+			output.data[ogp + 3] = input.data[igp + 3]; output.data[ogp + 4] = input.data[igp + 1]; output.data[ogp + 5] = input.data[igp + 4];
+			output.data[ogp + 6] = input.data[igp + 5]; output.data[ogp + 7] = input.data[igp + 4]; output.data[ogp + 8] = input.data[igp + 2];
 		}
 	}
 };
@@ -102,8 +102,8 @@ struct CopyAnisotropic2DConductivity: public CopyConductivity {
 		for (size_t gpindex = 0; gpindex < gps; ++gpindex) {
 			int igp = 4 * gpindex;
 			int ogp = 4 * gpindex;
-			this->output.data[ogp + 0] = this->input.data[igp + 0]; this->output.data[ogp + 1] = this->input.data[igp + 2];
-			this->output.data[ogp + 2] = this->input.data[igp + 3]; this->output.data[ogp + 3] = this->input.data[igp + 1];
+			output.data[ogp + 0] = input.data[igp + 0]; output.data[ogp + 1] = input.data[igp + 2];
+			output.data[ogp + 2] = input.data[igp + 3]; output.data[ogp + 3] = input.data[igp + 1];
 		}
 	}
 };
@@ -117,9 +117,9 @@ struct CopyAnisotropic3DConductivity: public CopyConductivity {
 		for (size_t gpindex = 0; gpindex < gps; ++gpindex) {
 			int igp = 9 * gpindex;
 			int ogp = 9 * gpindex;
-			this->output.data[ogp + 0] = this->input.data[igp + 0]; this->output.data[ogp + 1] = this->input.data[igp + 3]; this->output.data[ogp + 2] = this->input.data[igp + 5];
-			this->output.data[ogp + 3] = this->input.data[igp + 6]; this->output.data[ogp + 4] = this->input.data[igp + 1]; this->output.data[ogp + 5] = this->input.data[igp + 4];
-			this->output.data[ogp + 6] = this->input.data[igp + 8]; this->output.data[ogp + 7] = this->input.data[igp + 7]; this->output.data[ogp + 8] = this->input.data[igp + 2];
+			output.data[ogp + 0] = input.data[igp + 0]; output.data[ogp + 1] = input.data[igp + 3]; output.data[ogp + 2] = input.data[igp + 5];
+			output.data[ogp + 3] = input.data[igp + 6]; output.data[ogp + 4] = input.data[igp + 1]; output.data[ogp + 5] = input.data[igp + 4];
+			output.data[ogp + 6] = input.data[igp + 8]; output.data[ogp + 7] = input.data[igp + 7]; output.data[ogp + 8] = input.data[igp + 2];
 		}
 	}
 };
