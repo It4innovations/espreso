9a9872ecfb22254e012ac9407dfc957f9c4746f3
Ondrej Meca <ondrej.meca@gmail.com>
2021-10-06 09:17:38 +0200

    FIX: non-cartesian coordinate models

--- src/analysis/assembler/operators/conductivity.cpp
+++ src/analysis/assembler/operators/conductivity.cpp
@@ -16,44 +16,57 @@ namespace espreso {
 
 void thermalConductivity(AX_HeatTransfer &module)
 {
+	bool cooToGP = false;
+	for(size_t interval = 0; !cooToGP && interval < info::mesh->elements->eintervals.size(); ++interval) {
+		const MaterialConfiguration *mat = info::mesh->materials[info::mesh->elements->eintervals[interval].material];
+		if (mat->coordinate_system.type != CoordinateSystemConfiguration::TYPE::CARTESIAN) {
+			cooToGP = true;
+		}
+	}
+
+	if (module.coords.gp.data == nullptr && cooToGP) {
+		module.controller.addInput(module.coords.gp, module.coords.node);
+		for(size_t interval = 0; interval < info::mesh->elements->eintervals.size(); ++interval) {
+			const MaterialConfiguration *mat = info::mesh->materials[info::mesh->elements->eintervals[interval].material];
+			if (mat->coordinate_system.type == CoordinateSystemConfiguration::TYPE::CARTESIAN) {
+				module.coords.gp.isconst[interval] = true;
+				module.coords.gp.update[interval] = -1;
+			}
+		}
+	}
 	if (module.coords.gp.data == nullptr) {
-		// we need to compute coordinates in gps
+		module.controller.prepare(module.coords.gp);
 		for(size_t interval = 0; interval < info::mesh->elements->eintervals.size(); ++interval) {
 			const MaterialConfiguration *mat = info::mesh->materials[info::mesh->elements->eintervals[interval].material];
 			if (mat->coordinate_system.type != CoordinateSystemConfiguration::TYPE::CARTESIAN) {
 				if (info::mesh->dimension == 2) {
 					module.elementOps[interval].emplace_back(instantiate<AX_HeatTransfer::NGP, 2, FromNodesToGaussPoints>(interval, module.controller, module.integration.N, module.coords.node, module.coords.gp));
-					module.controller.addInput(module.cooSystem.angle2D, module.coords.gp);
 				}
 				if (info::mesh->dimension == 3) {
 					module.elementOps[interval].emplace_back(instantiate<AX_HeatTransfer::NGP, 3, FromNodesToGaussPoints>(interval, module.controller, module.integration.N, module.coords.node, module.coords.gp));
-					module.controller.addInput(module.cooSystem.angle3D, module.coords.gp);
 				}
-				break;
 			}
 		}
+	}
 
-		// save the memory for cartesian intervals
-		module.coords.gp.setConstness(true);
-		for(size_t interval = 0; interval < info::mesh->elements->eintervals.size(); ++interval) {
-			const MaterialConfiguration *mat = info::mesh->materials[info::mesh->elements->eintervals[interval].material];
-			if (mat->coordinate_system.type != CoordinateSystemConfiguration::TYPE::CARTESIAN) {
-				module.coords.gp.isconst[interval] = false;
-			}
+	if (cooToGP) {
+		if (info::mesh->dimension == 2) {
+			module.controller.addInput(module.cooSystem.angle2D, module.coords.gp);
 		}
-
-		module.controller.prepare(module.coords.gp);
+		if (info::mesh->dimension == 3) {
+			module.controller.addInput(module.cooSystem.angle3D, module.coords.gp);
+		}
 	}
 
 	if (info::mesh->dimension == 2) {
 		module.controller.addInput(module.cooSystem.angle2D, module.cooSystem.cartesian2D, module.cooSystem.cylindric);
-		module.controller.addInput(module.material.conductivity, module.cooSystem.angle2D);
 		module.controller.prepare(module.cooSystem.angle2D);
+		module.controller.addInput(module.material.conductivity, module.cooSystem.angle2D);
 	}
 	if (info::mesh->dimension == 3) {
 		module.controller.addInput(module.cooSystem.angle3D, module.cooSystem.cartesian3D, module.cooSystem.cylindric, module.cooSystem.spherical);
-		module.controller.addInput(module.material.conductivity, module.cooSystem.angle3D);
 		module.controller.prepare(module.cooSystem.angle3D);
+		module.controller.addInput(module.material.conductivity, module.cooSystem.angle3D);
 	}
 
 	module.controller.addInput(module.material.conductivityIsotropic, module.material.model.isotropic);
@@ -83,8 +96,7 @@ void thermalConductivity(AX_HeatTransfer &module)
 					module.elementOps[interval].emplace_back(instantiate<AX_HeatTransfer::NGP, CylindricalRotation2D>(interval, module.controller, module.coords.gp, module.cooSystem.cylindric, module.cooSystem.angle2D));
 					module.elementOps[interval].emplace_back(instantiate<AX_HeatTransfer::NGP, ConductivityRotation2D>(interval, module.controller, module.cooSystem.angle2D, module.material.conductivity));
 					break;
-				case CoordinateSystemConfiguration::TYPE::SPHERICAL: break;
-				default:
+				case CoordinateSystemConfiguration::TYPE::SPHERICAL:
 					break;
 				}
 			}
@@ -109,10 +121,9 @@ void thermalConductivity(AX_HeatTransfer &module)
 					module.elementOps[interval].emplace_back(instantiate<AX_HeatTransfer::NGP, CylindricalRotation3D>(interval, module.controller, module.coords.gp, module.cooSystem.cylindric, module.cooSystem.angle3D));
 					module.elementOps[interval].emplace_back(instantiate<AX_HeatTransfer::NGP, ConductivityRotation3D>(interval, module.controller, module.cooSystem.angle3D, module.material.conductivity));
 					break;
-				case CoordinateSystemConfiguration::TYPE::SPHERICAL: break;
+				case CoordinateSystemConfiguration::TYPE::SPHERICAL:
 					module.elementOps[interval].emplace_back(instantiate<AX_HeatTransfer::NGP, SphericalRotation3D>(interval, module.controller, module.coords.gp, module.cooSystem.spherical, module.cooSystem.angle3D));
 					module.elementOps[interval].emplace_back(instantiate<AX_HeatTransfer::NGP, ConductivityRotation3D>(interval, module.controller, module.cooSystem.angle3D, module.material.conductivity));
-					break;
 					break;
 				}
 			}
