e7e00f8b18dd6d40814e4a87786cb8b897154006
Ondrej Meca <ondrej.meca@vsb.cz>
2021-11-18 15:59:39 +0100

    TEST: remove ASCII fan_part test

--- benchmarks/input/fan_part/tested.py
+++ benchmarks/input/fan_part/tested.py
@@ -17,8 +17,7 @@ def by():
     # VTK is not tested since rouding errors in ASCII format
     path = os.path.join("/", "data", "espreso", "mesiotest", "fan_part")
     ansys = os.path.join(path, "fan_part.dat")
-    ensightBinary = os.path.join(path, "fan_partBinary.case")
-    ensightAscii = os.path.join(path, "fan_partAscii.case")
+    ensight = os.path.join(path, "fan_part.case")
     xdmf = os.path.join(path, "fan_part.xmf")
 
     os.path.exists(ansys)
@@ -26,7 +25,7 @@ def by():
     os.path.exists(ensightAscii)
     os.path.exists(xdmf)
     for p in range(8, 32):
-        for format, file in [ ("ANSYS_CDB", ansys), ("ENSIGHT", ensightBinary), ("ENSIGHT", ensightAscii), ("XDMF", xdmf) ]:
+        for format, file in [ ("ANSYS_CDB", ansys), ("ENSIGHT", ensight), ("XDMF", xdmf) ]:
             yield run, file, format, p
 
 def run(file, format, p):
