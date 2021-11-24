779a694b5ea466e6a2c270f69f259f0f794391bd
Ondrej Meca <ondrej.meca@vsb.cz>
2021-11-24 12:53:07 +0100

    TEST: add EnSight ASCII cube benchmark

---
+++ benchmarks/input/cube/ensight/binary/tested.py
@@ -0,0 +1,24 @@
+
+import os
+from nose.tools import istest
+
+from estest import ESPRESOTest
+
+def setup():
+    ESPRESOTest.path = os.path.dirname(__file__)
+    ESPRESOTest.args = [ "file", "decomposer" ]
+
+def teardown():
+    ESPRESOTest.clean()
+
+@istest
+def by():
+    for processes in range(1, 16):
+        for decomposer in [ "NONE", "PTSCOTCH", "HILBERT_CURVE" ]:
+            yield run, processes, decomposer
+
+def run(processes, decomposer):
+    ESPRESOTest.processes = processes
+    ESPRESOTest.args[0] = os.path.join(ESPRESOTest.path, "cube.case")
+    ESPRESOTest.args[1] = decomposer
+    ESPRESOTest.compare_mesh("espreso.log", ESPRESOTest.run())
