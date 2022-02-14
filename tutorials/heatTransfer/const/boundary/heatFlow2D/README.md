bce5e53c549014d4065b2957b7b8af2b2b96a8e7
Ondrej Meca <ondrej.meca@gmail.com>
2022-02-14 10:17:14 +0100

    CASE: heat flow 2D

---
+++ tutorials/heatTransfer/const/boundary/heatFlow2D/README.md
@@ -0,0 +1,24 @@
+
+# HEAT FLOW 2D
+
+## Description
+
+Material parameters:
+ - ISOTROPIC
+ - Conductivity = 5
+
+Boundary condition settings:
+ - GREEN: heat flow = 300
+ - RED: fixed temperature = 100
+
+![description](description.png)
+
+# Run
+
+```
+mpirun -n 4 espreso
+```
+
+## Solution
+
+![solution](solution.png)
---
+++ tutorials/heatTransfer/const/boundary/heatFlow2D/tested.py
@@ -0,0 +1,22 @@
+
+import os
+from nose.tools import istest
+
+from estest import ESPRESOTest
+
+def setup():
+    ESPRESOTest.path = os.path.dirname(__file__)
+    ESPRESOTest.args = [ "etype", 2, 2, 2, 2, 10, 10 ]
+
+def teardown():
+    ESPRESOTest.clean()
+
+@istest
+def by():
+    for etype in [ "SQUARE4", "SQUARE8", "TRIANGLE3", "TRIANGLE6" ]:
+        yield run, etype
+
+def run(etype):
+    ESPRESOTest.args[0] = etype
+    ESPRESOTest.run()
+    ESPRESOTest.compare_emr("espreso.emr")
