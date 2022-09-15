f512f84970093b051d0183a9a309ed3a000da705
Ondrej Meca <ondrej.meca@vsb.cz>
2022-09-15 14:29:16 +0200

    TEST: sparse solver on single core

--- benchmarks/performance/DOF1RealSymmetricPositiveDefinite/singleCore/test.py
+++ benchmarks/performance/DOF1RealSymmetricPositiveDefinite/singleCore/test.py
@@ -1,21 +1,26 @@
 
-import os, math, unittest
+import os, math, unittest, itertools
 from estest import ESPRESOTest
 
 results = list()
 
 functions = [
-    ("FETI: TFETI SYMBOLIC FACTORIZATION", "avg"),
-    ("FETI[SET] TOTAL DURATION", "avg"),
+    ("B1 ROWS", "avg"),
     ("K+ ROWS", "avg"),
     ("K+ NNZ", "avg"),
     ("K+ FACTORS NNZ", "avg"),
+    ("FETI: TFETI SYMBOLIC FACTORIZATION", "avg"),
     ("FETI: TFETI NUMERICAL FACTORIZATION", "avg"),
     ("FETI: TFETI (B * K+ * B') ASSEMBLED", "avg"),
-    ("FETI[UPDATE] TOTAL DURATION", "avg"),
     ("cpg: apply F [~]", "avg"),
-    ("cpg TOTAL DURATION", "avg")
+    ("ITERATIONS TOTAL", "iterations"),
+    ("cpg TOTAL DURATION", "avg"),
+    ("FETI[SET] TOTAL DURATION", "avg"),
+    ("FETI[UPDATE] TOTAL DURATION", "avg"),
+    ("FETI[SOLVE] TOTAL DURATION", "avg"),
     ]
+
+testcases = [ (x, y, z) for x, y, z in itertools.product([ "UNIFORM", "METIS" ], [ "IMPLICIT_TFETI", "EXPLICIT_TFETI" ], [ "FULL", "PARTIAL" ]) ]
 
 def report():
     def pick(uniform_domains, method, partial_dual):
@@ -25,82 +30,91 @@ def report():
                 measurement.append(((key[0], key[1]), values))
         return measurement
 
+    ESPRESOTest.parse(results, "B1 ROWS", ["avg", "min", "max"])
     ESPRESOTest.parse(results, "K+ ROWS", ["avg", "min", "max"])
     ESPRESOTest.parse(results, "K+ NNZ", ["avg", "min", "max"])
     ESPRESOTest.parse(results, "K+ FACTORS NNZ", ["avg", "min", "max"])
-    ESPRESOTest.report_mesurement("IMPLICIT_UNIFORM_FULL", pick("UNIFORM", "IMPLICIT_TFETI", "FULL"), functions)
-    ESPRESOTest.report_mesurement("EXPLICIT_UNIFORM_FULL", pick("UNIFORM", "EXPLICIT_TFETI", "FULL"), functions)
-    ESPRESOTest.report_mesurement("EXPLICIT_UNIFORM_PARTIAL", pick("UNIFORM", "EXPLICIT_TFETI", "PARTIAL"), functions)
-    ESPRESOTest.report_mesurement("IMPLICIT_METIS_FULL", pick("METIS", "IMPLICIT_TFETI", "FULL"), functions)
-    ESPRESOTest.report_mesurement("EXPLICIT_METIS_FULL", pick("METIS", "EXPLICIT_TFETI", "FULL"), functions)
-    ESPRESOTest.report_mesurement("EXPLICIT_METIS_PARTIAL", pick("METIS", "EXPLICIT_TFETI", "PARTIAL"), functions)
+    ESPRESOTest.parse(results, "ITERATIONS TOTAL", ["iterations"])
+    for uniform_domains, method, partial_dual in testcases:
+        ESPRESOTest.report_mesurement("_".join([method, partial_dual, uniform_domains]), pick(uniform_domains, method, partial_dual), functions)
 
 class TotalFETIOperator2D(unittest.TestCase):
 
     def setUp(self):
         ESPRESOTest.path = os.path.dirname(__file__)
         ESPRESOTest.processes = 1
+        ESPRESOTest.set_threads(1)
         ESPRESOTest.ecf = "espreso2D.ecf"
-        ESPRESOTest.args = [ "etype", 2, 2, "uniform_domains", "TFETI", "restricted_dual" ]
+        ESPRESOTest.args = [ "etype", "ex", "ey", "uniform_domains", "TFETI", "restricted_dual" ]
 
     def tearDown(self):
-        report()
+        # report()
         ESPRESOTest.clean()
 
     def test_performance(self):
-        for etype in [ "SQUARE4", "SQUARE8", "TRIANGLE3", "TRIANGLE6" ]:
-            for size in range(7, 14):
-                yield run2D, etype, int(math.sqrt(2 ** size) - 1), "UNIFORM", "IMPLICIT_TFETI", "FULL"
-                yield run2D, etype, int(math.sqrt(2 ** size) - 1), "UNIFORM", "EXPLICIT_TFETI", "FULL"
-                yield run2D, etype, int(math.sqrt(2 ** size) - 1), "UNIFORM", "EXPLICIT_TFETI", "PARTIAL"
-                yield run2D, etype, int(math.sqrt(2 ** size) - 1), "METIS", "IMPLICIT_TFETI", "FULL"
-                yield run2D, etype, int(math.sqrt(2 ** size) - 1), "METIS", "EXPLICIT_TFETI", "FULL"
-                yield run2D, etype, int(math.sqrt(2 ** size) - 1), "METIS", "EXPLICIT_TFETI", "PARTIAL"
+        for etype, mult in [ ("SQUARE4", 1), ("SQUARE8", 2), ("TRIANGLE3", 1), ("TRIANGLE6", 2) ]:
+            ex = ey = 8
+            while ex * ey < 66000:
+                for uniform_domains, method, partial_dual in testcases:
+                    yield run2D, etype, ex // mult, ey // mult, uniform_domains, method, partial_dual
+                if ex == ey:
+                    ey = 2 * ey
+                else:
+                    ex = 2 * ex
+                print(ex, ey)
+
+def run2D(etype, ex, ey, uniform_domains, method, partial_dual):
+    return
+    ESPRESOTest.args[0] = etype
+    ESPRESOTest.args[1] = ex - 1
+    ESPRESOTest.args[2] = ey - 1
+    ESPRESOTest.args[3] = uniform_domains == "UNIFORM"
+    ESPRESOTest.args[4] = method
+    ESPRESOTest.args[5] = partial_dual == "PARTIAL"
+    results.append(((etype, ex * ey, uniform_domains, method, partial_dual), ESPRESOTest.extract(ESPRESOTest.run(), [e[0] for e in functions])))
+
+run2D.performance = 1
+run2D.generator = 1
 
 class TotalFETIOperator3D(unittest.TestCase):
 
     def setUp(self):
         ESPRESOTest.path = os.path.dirname(__file__)
         ESPRESOTest.processes = 1
+        ESPRESOTest.set_threads(1)
         ESPRESOTest.ecf = "espreso3D.ecf"
-        ESPRESOTest.args = [ "etype", 2, 2, 2, "uniform_domains", "TFETI", "restricted_dual" ]
+        ESPRESOTest.args = [ "etype", "ex", "ey", "ez", "uniform_domains", "TFETI", "restricted_dual" ]
 
     def tearDown(self):
         report()
         ESPRESOTest.clean()
 
     def test_performance(self):
-        for etype in [ "HEXA8", "HEXA20", "TETRA4", "TETRA10", "PRISMA6", "PRISMA15", "PYRAMID5", "PYRAMID13" ]:
-            for size in range(7, 14):
-                yield run3D, etype, int(math.pow(2 ** size, 1./3) - 1), "UNIFORM", "IMPLICIT_TFETI", "FULL"
-                yield run3D, etype, int(math.pow(2 ** size, 1./3) - 1), "UNIFORM", "EXPLICIT_TFETI", "FULL"
-                yield run3D, etype, int(math.pow(2 ** size, 1./3) - 1), "UNIFORM", "EXPLICIT_TFETI", "PARTIAL"
-                yield run3D, etype, int(math.pow(2 ** size, 1./3) - 1), "METIS", "IMPLICIT_TFETI", "FULL"
-                yield run3D, etype, int(math.pow(2 ** size, 1./3) - 1), "METIS", "EXPLICIT_TFETI", "FULL"
-                yield run3D, etype, int(math.pow(2 ** size, 1./3) - 1), "METIS", "EXPLICIT_TFETI", "PARTIAL"
+        # for etype in [ "HEXA8", "HEXA20", "TETRA4", "TETRA10", "PRISMA6", "PRISMA15", "PYRAMID5", "PYRAMID13" ]:
+        for etype in [ "HEXA8" ]:
+            ex = ey = ez = 4
+            while ex * ey * ez < 66000:
+                print(testcases)
+                for uniform_domains, method, partial_dual in testcases:
+                    yield run3D, etype, ex, ey, ez, uniform_domains, method, partial_dual
+                if ex == ey and ey == ez:
+                    ez = 2 * ez
+                else:
+                    if ex == ey:
+                        ey = 2 * ey
+                    else:
+                        ex = 2 * ex
+                print(ex, ey, ez)
 
-def run2D(etype, size, uniform_domains, method, partial_dual):
-    ESPRESOTest.set_threads(1)
+def run3D(etype, ex, ey, ez, uniform_domains, method, partial_dual):
     ESPRESOTest.args[0] = etype
-    ESPRESOTest.args[1] = size
-    ESPRESOTest.args[2] = size
-    ESPRESOTest.args[3] = uniform_domains == "UNIFORM"
-    ESPRESOTest.args[4] = method
-    ESPRESOTest.args[5] = partial_dual == "PARTIAL"
-    results.append(((etype, (size + 1) * (size + 1), uniform_domains, method, partial_dual), ESPRESOTest.extract(ESPRESOTest.run(), [e[0] for e in functions])))
-
-def run3D(etype, size, uniform_domains, method, partial_dual):
-    ESPRESOTest.set_threads(1)
-    ESPRESOTest.args[0] = etype
-    ESPRESOTest.args[1] = size
-    ESPRESOTest.args[2] = size
-    ESPRESOTest.args[3] = size
+    ESPRESOTest.args[1] = ex - 1
+    ESPRESOTest.args[2] = ey - 1
+    ESPRESOTest.args[3] = ez - 1
     ESPRESOTest.args[4] = uniform_domains == "UNIFORM"
     ESPRESOTest.args[5] = method
     ESPRESOTest.args[6] = partial_dual == "PARTIAL"
-    results.append(((etype, (size + 1) * (size + 1) * (size + 1), uniform_domains, method, partial_dual), ESPRESOTest.extract(ESPRESOTest.run(), [e[0] for e in functions])))
+    results.append(((etype, ex * ey * ez, uniform_domains, method, partial_dual), ESPRESOTest.extract(ESPRESOTest.run(), [e[0] for e in functions])))
 
-run2D.performance = 1
-run2D.generator = 1
 run3D.performance = 1
 run3D.generator = 1
