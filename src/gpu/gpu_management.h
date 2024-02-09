5462a9b0d597c230a362cbd54ef409773eabb0c9
Jakub Homola <jakub.homola@vsb.cz>
2024-02-09 18:16:16 +0100

    ENH: gpu matrix print functions

--- src/gpu/gpu_management.h
+++ src/gpu/gpu_management.h
@@ -4,6 +4,7 @@
 
 #include <vector>
 #include <functional>
+#include <cstring>
 
 #include "math/primitives/vector_dense.h"
 #include "math/primitives/matrix_dense.h"
@@ -149,6 +150,88 @@ namespace mgm {
             return reinterpret_cast<T*>(allocate(count * sizeof(T)));
         }
     };
+
+    template<typename T, typename I, typename A>
+    void print_matrix_dense(queue & q, Matrix_Dense<T,I,A> & matrix, const char * name = "")
+    {
+        if constexpr(A::is_data_host_accessible)
+        {
+            printf("Dense matrix %s, size %lldx%lld, ld %lld\n", name, (long long)matrix.nrows, (long long)matrix.ncols, (long long)matrix.get_ld());
+            for(I r = 0; r < matrix.nrows; r++)
+            {
+                for(I c = 0; c < matrix.ncols; c++)
+                {
+                    if constexpr(std::is_floating_point_v<T>)
+                    {
+                        double v = (double)matrix.vals[r * matrix.get_ld() + c];
+                        char str[100];
+                        snprintf(str, sizeof(str), "%+11.3e", v);
+                        if(strstr(str, "nan") != nullptr) printf("   nan      ");
+                        else if(strstr(str, "inf") != nullptr) printf("  %cinf      ", v > 0 ? '+' : '-');
+                        else if(v == 0) printf("   0        ");
+                        else printf(" %+11.3e", v);
+                    }
+                    if constexpr(std::is_integral_v<T>) printf(" %+11lld", (long long)matrix.vals[r * matrix.get_ld() + c]);
+                }
+                printf("\n");
+            }
+            fflush(stdout);
+        }
+        else if constexpr(A::is_data_device_accessible)
+        {
+            Matrix_Dense<T,I,Ah> matrix_host;
+            matrix_host.resize(matrix);
+            copy_submit_d2h(q, matrix_host, matrix);
+            queue_wait(q);
+            print_matrix_dense(q, matrix_host, name);
+        }
+        else
+        {
+            static_assert(true, "weird matrix with inaccessible data");
+        }
+    }
+
+    template<typename T, typename I, typename A>
+    void print_matrix_csr_as_dense(queue & q, Matrix_CSR<T,I,A> & matrix, const char * name = "")
+    {
+        if constexpr(A::is_data_host_accessible)
+        {
+            printf("CSR matrix %s, size %lldx%lld, nnz %lld\n", name, (long long)matrix.nrows, (long long)matrix.ncols, (long long)matrix.nnz);
+            for(I r = 0; r < matrix.nrows; r++)
+            {
+                I start = matrix.rows[r];
+                I end = matrix.rows[r+1];
+                I curr_col = 0;
+                for(I i = start; i <= end; i++)
+                {
+                    I col = matrix.ncols;
+                    if(i < end) col = matrix.cols[i];
+                    for(I c = curr_col; c < col; c++) printf("    .       ");
+                    if(i < end)
+                    {
+                        double val = matrix.vals[i];
+                        if(val == 0) printf("    0       ");
+                        else printf(" %+11.3e", val);
+                        curr_col = col + 1;
+                    }
+                }
+                printf("\n");
+            }
+            fflush(stdout);
+        }
+        else if constexpr(A::is_data_device_accessible)
+        {
+            Matrix_CSR<T,I,Ah> matrix_host;
+            matrix_host.resize(matrix);
+            copy_submit_d2h(q, matrix_host, matrix);
+            queue_wait(q);
+            print_matrix_csr_as_dense(q, matrix_host, name);
+        }
+        else
+        {
+            static_assert(true, "weird matrix with inaccessible data");
+        }
+    }
 
 }
 }
