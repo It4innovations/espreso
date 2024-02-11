77616dd1eaba4483225f4580223310f051a23d6c
Ondrej Meca <ondrej.meca@gmail.com>
2024-02-11 23:10:31 +0100

    TEST: contact solver by MPRGP

---
+++ tests/feti/contact/transient/iterativeSolver/linearElasticity3D/test.py
@@ -0,0 +1,29 @@
+
+import os, unittest
+from estest import ESPRESOTest
+
+cases = [
+    ("MPRGP", "     NONE", 980),
+    ]
+
+class Assembler(unittest.TestCase):
+
+    def setUp(self):
+        ESPRESOTest.path = os.path.dirname(__file__)
+        ESPRESOTest.args = [ "HEXA8", 2, 2, 1, 2, 2, 2, 4, 4, 8, "MPRGP", "NONE", 980 ]
+        ESPRESOTest.processes = 4
+        ESPRESOTest.set_threads(2)
+
+    def tearDown(self):
+        ESPRESOTest.clean()
+
+    def test_feti(self):
+        for solver, prec, iterations in cases:
+            yield run, solver, prec, iterations
+
+def run(solver, prec, iterations):
+    ESPRESOTest.args[10] = solver
+    ESPRESOTest.args[11] = prec
+    ESPRESOTest.args[12] = iterations
+    ESPRESOTest.run()
+    ESPRESOTest.compare_emr("espreso.emr")
