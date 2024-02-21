f0429bafc45ee461b4aae3abdafede6ac01409e8
Jakub Homola <jakub.homola@vsb.cz>
2024-02-21 18:58:28 +0100

    RF: spaces->tabs to make it consistent

--- src/config/ecf/linearsolver/dual_operator_explicit_gpu_config.h
+++ src/config/ecf/linearsolver/dual_operator_explicit_gpu_config.h
@@ -8,73 +8,73 @@ namespace espreso {
 
 struct DualOperatorExplicitGpuConfig: public ECFDescription {
 
-    enum class CONCURRENCY {
-        DEFAULT,
-        PARALLEL,
-        SEQ_CONTINUE,
-        SEQ_WAIT
-    };
+	enum class CONCURRENCY {
+		DEFAULT,
+		PARALLEL,
+		SEQ_CONTINUE,
+		SEQ_WAIT
+	};
 
-    enum class MATRIX_STORAGE {
-        DEFAULT,
-        SPARSE,
-        DENSE
-    };
+	enum class MATRIX_STORAGE {
+		DEFAULT,
+		SPARSE,
+		DENSE
+	};
 
-    enum class TRSM1_SOLVE_TYPE {
-        DEFAULT,
-        L,
-        LHH
-    };
+	enum class TRSM1_SOLVE_TYPE {
+		DEFAULT,
+		L,
+		LHH
+	};
 
-    enum class TRSM2_SOLVE_TYPE {
-        DEFAULT,
-        U,
-        UHH
-    };
+	enum class TRSM2_SOLVE_TYPE {
+		DEFAULT,
+		U,
+		UHH
+	};
 
-    enum class MATRIX_ORDER {
-        DEFAULT,
-        ROW_MAJOR,
-        COL_MAJOR
-    };
+	enum class MATRIX_ORDER {
+		DEFAULT,
+		ROW_MAJOR,
+		COL_MAJOR
+	};
 
-    enum class PATH_IF_HERMITIAN {
-        DEFAULT,
-        TRSM,
-        HERK
-    };
+	enum class PATH_IF_HERMITIAN {
+		DEFAULT,
+		TRSM,
+		HERK
+	};
 
-    enum class TRIANGLE_MATRIX_SHARING {
-        DEFAULT,
-        PRIVATE,
-        SHARED
-    };
+	enum class TRIANGLE_MATRIX_SHARING {
+		DEFAULT,
+		PRIVATE,
+		SHARED
+	};
 
-    enum class QUEUE_COUNT {
-        DEFAULT,
-        PER_THREAD,
-        PER_DOMAIN
-    };
+	enum class QUEUE_COUNT {
+		DEFAULT,
+		PER_THREAD,
+		PER_DOMAIN
+	};
 
-    enum class DEVICE {
-        DEFAULT,
-        CPU,
-        GPU
-    };
+	enum class DEVICE {
+		DEFAULT,
+		CPU,
+		GPU
+	};
 
-    CONCURRENCY concurrency_set;
-    CONCURRENCY concurrency_update;
-    CONCURRENCY concurrency_apply;
-    MATRIX_STORAGE trsm1_factor_storage;
-    MATRIX_STORAGE trsm2_factor_storage;
-    TRSM1_SOLVE_TYPE trsm1_solve_type;
-    TRSM2_SOLVE_TYPE trsm2_solve_type;
-    MATRIX_ORDER trsm_rhs_sol_order;
-    PATH_IF_HERMITIAN path_if_hermitian;
-    TRIANGLE_MATRIX_SHARING f_sharing_if_hermitian;
-    QUEUE_COUNT queue_count;
-    DEVICE apply_scatter_gather_where;
+	CONCURRENCY concurrency_set;
+	CONCURRENCY concurrency_update;
+	CONCURRENCY concurrency_apply;
+	MATRIX_STORAGE trsm1_factor_storage;
+	MATRIX_STORAGE trsm2_factor_storage;
+	TRSM1_SOLVE_TYPE trsm1_solve_type;
+	TRSM2_SOLVE_TYPE trsm2_solve_type;
+	MATRIX_ORDER trsm_rhs_sol_order;
+	PATH_IF_HERMITIAN path_if_hermitian;
+	TRIANGLE_MATRIX_SHARING f_sharing_if_hermitian;
+	QUEUE_COUNT queue_count;
+	DEVICE apply_scatter_gather_where;
 
 	DualOperatorExplicitGpuConfig();
 
