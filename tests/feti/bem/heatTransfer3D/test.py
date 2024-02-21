6c2d67bc44de85a63652406d1640f2726e53a3a3
Ondrej Meca <ondrej.meca@gmail.com>
2024-02-21 22:13:15 +0100

    TEST: add tests to BEM

---
+++ tests/feti/bem/heatTransfer3D/test.py
@@ -0,0 +1,22 @@
+
+import os, unittest
+from estest import ESPRESOTest
+
+class Assembler(unittest.TestCase):
+
+    def startTest(self, event):
+        event.test._testFunc = dummy
+        event.handled = True
+
+    def setUp(self):
+        ESPRESOTest.path = os.path.dirname(__file__)
+        ESPRESOTest.args = [ 2, 2, 1, 2, 2, 2, 4, 4, 8 ]
+        ESPRESOTest.processes = 4
+        ESPRESOTest.set_threads(2)
+
+    def tearDown(self):
+        ESPRESOTest.clean()
+
+    def test_feti(self):
+        ESPRESOTest.run()
+        ESPRESOTest.compare_emr("espreso.emr")
