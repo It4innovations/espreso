f7b2145296627284163017f65a8abbcfa92df5d8
Ondrej Meca <ondrej.meca@gmail.com>
2024-03-06 21:57:22 +0100

    TEST: add test to transient linear elasticity 3D

--- tests/physicalSolver/transient/linear/structuralMechanics3D/test.py
+++ tests/physicalSolver/transient/linear/structuralMechanics3D/test.py
@@ -6,7 +6,7 @@ class PhysicalSolver(unittest.TestCase):
 
     def setUp(self):
         ESPRESOTest.path = os.path.dirname(__file__)
-        ESPRESOTest.args = [ "HEXA8", 2, 2, 1, 2, 2, 2, 4, 4, 8, "FETI" ]
+        ESPRESOTest.args = [ "HEXA8", 2, 1, 2, 4, 2, 2, 10, 4, 5, "FETI" ]
         ESPRESOTest.processes = 4
         ESPRESOTest.set_threads(2)
 
