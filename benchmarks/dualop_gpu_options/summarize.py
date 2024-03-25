cd17dd6546243c2f467976a8cfb5f63b4ef608c6
Jakub Homola <jakub.homola@vsb.cz>
2024-03-25 11:10:44 +0100

    FIX: summarize script adjustments

--- benchmarks/dualop_gpu_options/summarize.py
+++ benchmarks/dualop_gpu_options/summarize.py
@@ -34,8 +34,7 @@ if not os.path.exists(basedir + "/summarize.py"):
 # copy or put symlinks to this folder
 results_dir = basedir + "/results_to_summarize"
 
-# datestr = datetime.now().strftime("%Y%m%d_%H%M%S")
-datestr = "20240324_232401"
+datestr = datetime.now().strftime("%Y%m%d_%H%M%S")
 summ_dir = basedir + "/summary/" + datestr
 os.makedirs(summ_dir, exist_ok=True)
 
@@ -54,99 +53,103 @@ runs_finished = 0
 
 
 
-# outfile = summ_dir + "/set.csv"
-# outstring = io.StringIO()
-# cells_info = ["id", "str", "machine", "tool", "problem", "element_type", "domains_x", "domains_y", "domains_z", "n_domains_total", "elements_x", "elements_y", "elements_z", "n_elements_total", "concurrency_set", "concurrency_update", "concurrency_apply", "factor_symmetry_fake", "uniform_clusters_domains", "trsm1_factor_storage", "trsm2_factor_storage", "trsm1_solve_type", "trsm2_solve_type", "trsm_rhs_sol_order", "path_if_hermitian", "f_sharing_if_hermitian", "apply_scatter_gather_where", "transpose_where"]
-# cells_timers = ["total", "gpuinit", "gpuset", "vecresize", "sizecalc", "gpucreate", "mainloop_outer", "mainloop_inner", "Kreg_combine", "solver_commit", "fact_symbolic", "descriptors", "buffersize", "alloc", "alloc_host", "alloc_device", "setpointers", "Bperm", "get_factors", "extract", "trans_cpu", "copyin", "kernels_preprocess", "trans_gpu", "trs1", "trs2", "gemm", "applystuff", "poolalloc", "wait"]
-# outstring.write(";".join(cells_info))
-# outstring.write(";")
-# outstring.write("error;timeout")
-# outstring.write(";")
-# outstring.write(";".join(cells_timers))
-# outstring.write("\n")
-# for dir_name in os.listdir(results_dir):
-#     dir_path = results_dir + "/" + dir_name
-#     for run_id in os.listdir(dir_path + "/setupdate"):
-#         run_path = dir_path + "/setupdate/" + run_id
-#         info_lines = read_file_to_string(run_path + "/info.txt").split("\n")
-#         for field in cells_info:
-#             if field == "id" or field == "str" or field == "n_domains_total" or field == "n_elements_total":
-#                 val = ""
-#             elif field == "problem":
-#                 problem = list(filter(lambda line: "ecf_file" in line, info_lines))[0].split(".")[1]
-#                 val = problem
-#             else:
-#                 val = list(filter(lambda line: field in line, info_lines))[0].split(" ")[1]
-#             outstring.write(val)
-#             outstring.write(";")
+outfile = summ_dir + "/set.csv"
+outstring = io.StringIO()
+cells_info = ["id", "str", "run_id", "machine", "tool", "problem", "element_type", "domains_x", "domains_y", "domains_z", "n_domains_total", "elements_x", "elements_y", "elements_z", "n_elements_total", "concurrency_set", "concurrency_update", "concurrency_apply", "factor_symmetry_fake", "uniform_clusters_domains", "trsm1_factor_storage", "trsm2_factor_storage", "trsm1_solve_type", "trsm2_solve_type", "trsm_rhs_sol_order", "path_if_hermitian", "f_sharing_if_hermitian", "apply_scatter_gather_where", "transpose_where"]
+cells_timers = ["total", "gpuinit", "gpuset", "vecresize", "sizecalc", "gpucreate", "mainloop_outer", "mainloop_inner", "Kreg_combine", "solver_commit", "fact_symbolic", "descriptors", "buffersize", "alloc", "alloc_host", "alloc_device", "setpointers", "Bperm", "get_factors", "extract", "trans_cpu", "copyin", "kernels_preprocess", "trans_gpu", "trs1", "trs2", "gemm", "applystuff", "poolalloc", "wait"]
+outstring.write(";".join(cells_info))
+outstring.write(";;")
+outstring.write("error;timeout")
+outstring.write(";;")
+outstring.write(";".join(cells_timers))
+outstring.write("\n")
+for dir_name in os.listdir(results_dir):
+    dir_path = results_dir + "/" + dir_name
+    for run_id in os.listdir(dir_path + "/setupdate"):
+        run_path = dir_path + "/setupdate/" + run_id
+        info_lines = read_file_to_string(run_path + "/info.txt").split("\n")
+        for field in cells_info:
+            if field == "id" or field == "str" or field == "n_domains_total" or field == "n_elements_total":
+                val = ""
+            elif field == "run_id":
+                val = run_id
+            elif field == "problem":
+                problem = list(filter(lambda line: "ecf_file" in line, info_lines))[0].split(".")[1]
+                val = problem
+            else:
+                val = list(filter(lambda line: field in line, info_lines))[0].split(" ")[1]
+            outstring.write(val)
+            outstring.write(";")
 
-#         outstring.write(";")
-#         outstring.write("\"" + read_file_to_string(run_path + "/last/espreso." + problem + ".err").replace("\n", ";").replace("\"", "\"\"") + "\";")
-#         outstring.write("\"" + read_file_to_string(run_path + "/timeout.txt").replace("\n", ";").replace("\"", "\"\"") + "\";")
-#         outstring.write(";")
+        outstring.write(";")
+        outstring.write("\"" + read_file_to_string(run_path + "/last/espreso." + problem + ".err").split("\0")[0].replace("\n", ";").replace("\"", "\"\"") + "\";")
+        outstring.write("\"" + read_file_to_string(run_path + "/timeout.txt").replace("\n", ";").replace("\"", "\"\"") + "\";")
+        outstring.write(";")
     
-#         update_lines = list(filter(lambda line: "Set" in line and "rank" in line, read_file_to_string(run_path + "/last/espreso." + problem + ".log").split("\n")))
-#         for field in cells_timers:
-#             lines = list(filter(lambda line: field in line, update_lines))
-#             if len(lines) > 0:
-#                 time = lines[0][70:90].replace(" ", "")
-#                 outstring.write(time)
-#             outstring.write(";")
-#         outstring.write("\n")
-#         runs_finished += 1
-#         if runs_finished % 1000 == 0:
-#             print("Progress: ", runs_finished, "/", runs_total)
-# write_string_to_file(outfile, outstring.getvalue())
-# outstring.close()
+        update_lines = list(filter(lambda line: "Set" in line and "rank" in line, read_file_to_string(run_path + "/last/espreso." + problem + ".log").split("\n")))
+        for field in cells_timers:
+            lines = list(filter(lambda line: field in line, update_lines))
+            if len(lines) > 0:
+                time = lines[0][70:90].replace(" ", "").replace("-nan", "").replace("nan", "")
+                outstring.write(time)
+            outstring.write(";")
+        outstring.write("\n")
+        runs_finished += 1
+        if runs_finished % 1000 == 0:
+            print("Progress: ", runs_finished, "/", runs_total)
+write_string_to_file(outfile, outstring.getvalue())
+outstring.close()
 
 
 
 
 
-# outfile = summ_dir + "/update.csv"
-# outstring = io.StringIO()
-# cells_info = ["id", "str", "machine", "tool", "problem", "element_type", "domains_x", "domains_y", "domains_z", "n_domains_total", "elements_x", "elements_y", "elements_z", "n_elements_total", "concurrency_set", "concurrency_update", "concurrency_apply", "factor_symmetry_fake", "uniform_clusters_domains", "trsm1_factor_storage", "trsm2_factor_storage", "trsm1_solve_type", "trsm2_solve_type", "trsm_rhs_sol_order", "path_if_hermitian", "f_sharing_if_hermitian", "apply_scatter_gather_where", "transpose_where"]
-# cells_timers = ["total", "mainloop_outer", "mainloop_inner", "Kreg_combine", "solver_commit", "fact_numeric", "get_factors", "extract", "trans_cpu", "allocinpool", "setpointers", "copyin", "trans_gpu", "descr_update", "descr_update_trs1", "descr_update_trs2", "sp2dn", "kernels_compute", "trs1", "trs2", "gemm", "fcopy", "syrk", "freeinpool", "compute_d", "wait"]
-# outstring.write(";".join(cells_info))
-# outstring.write(";")
-# outstring.write("error;timeout")
-# outstring.write(";")
-# outstring.write(";".join(cells_timers))
-# outstring.write("\n")
-# for dir_name in os.listdir(results_dir):
-#     dir_path = results_dir + "/" + dir_name
-#     for run_id in os.listdir(dir_path + "/setupdate"):
-#         run_path = dir_path + "/setupdate/" + run_id
-#         info_lines = read_file_to_string(run_path + "/info.txt").split("\n")
-#         for field in cells_info:
-#             if field == "id" or field == "str" or field == "n_domains_total" or field == "n_elements_total":
-#                 val = ""
-#             elif field == "problem":
-#                 problem = list(filter(lambda line: "ecf_file" in line, info_lines))[0].split(".")[1]
-#                 val = problem
-#             else:
-#                 val = list(filter(lambda line: field in line, info_lines))[0].split(" ")[1]
-#             outstring.write(val)
-#             outstring.write(";")
+outfile = summ_dir + "/update.csv"
+outstring = io.StringIO()
+cells_info = ["id", "str", "run_id", "machine", "tool", "problem", "element_type", "domains_x", "domains_y", "domains_z", "n_domains_total", "elements_x", "elements_y", "elements_z", "n_elements_total", "concurrency_set", "concurrency_update", "concurrency_apply", "factor_symmetry_fake", "uniform_clusters_domains", "trsm1_factor_storage", "trsm2_factor_storage", "trsm1_solve_type", "trsm2_solve_type", "trsm_rhs_sol_order", "path_if_hermitian", "f_sharing_if_hermitian", "apply_scatter_gather_where", "transpose_where"]
+cells_timers = ["total", "mainloop_outer", "mainloop_inner", "Kreg_combine", "solver_commit", "fact_numeric", "get_factors", "extract", "trans_cpu", "allocinpool", "setpointers", "copyin", "trans_gpu", "descr_update", "descr_update_trs1", "descr_update_trs2", "sp2dn", "kernels_compute", "trs1", "trs2", "gemm", "fcopy", "syrk", "freeinpool", "compute_d", "wait"]
+outstring.write(";".join(cells_info))
+outstring.write(";;")
+outstring.write("error;timeout")
+outstring.write(";;")
+outstring.write(";".join(cells_timers))
+outstring.write("\n")
+for dir_name in os.listdir(results_dir):
+    dir_path = results_dir + "/" + dir_name
+    for run_id in os.listdir(dir_path + "/setupdate"):
+        run_path = dir_path + "/setupdate/" + run_id
+        info_lines = read_file_to_string(run_path + "/info.txt").split("\n")
+        for field in cells_info:
+            if field == "id" or field == "str" or field == "n_domains_total" or field == "n_elements_total":
+                val = ""
+            elif field == "run_id":
+                val = run_id
+            elif field == "problem":
+                problem = list(filter(lambda line: "ecf_file" in line, info_lines))[0].split(".")[1]
+                val = problem
+            else:
+                val = list(filter(lambda line: field in line, info_lines))[0].split(" ")[1]
+            outstring.write(val)
+            outstring.write(";")
 
-#         outstring.write(";")
-#         outstring.write("\"" + read_file_to_string(run_path + "/last/espreso." + problem + ".err").replace("\n", ";").replace("\"", "\"\"") + "\";")
-#         outstring.write("\"" + read_file_to_string(run_path + "/timeout.txt").replace("\n", ";").replace("\"", "\"\"") + "\";")
-#         outstring.write(";")
+        outstring.write(";")
+        outstring.write("\"" + read_file_to_string(run_path + "/last/espreso." + problem + ".err").split("\0")[0].replace("\n", ";").replace("\"", "\"\"") + "\";")
+        outstring.write("\"" + read_file_to_string(run_path + "/timeout.txt").replace("\n", ";").replace("\"", "\"\"") + "\";")
+        outstring.write(";")
     
-#         update_lines = list(filter(lambda line: "Update" in line and "rank" in line, read_file_to_string(run_path + "/last/espreso." + problem + ".log").split("\n")))
-#         for field in cells_timers:
-#             lines = list(filter(lambda line: field in line, update_lines))
-#             if len(lines) >= 2:
-#                 time = lines[1][70:90].replace(" ", "")
-#                 outstring.write(time)
-#             outstring.write(";")
-#         outstring.write("\n")
-#         runs_finished += 1
-#         if runs_finished % 1000 == 0:
-#             print("Progress: ", runs_finished, "/", runs_total)
-# write_string_to_file(outfile, outstring.getvalue())
-# outstring.close()
+        update_lines = list(filter(lambda line: "Update" in line and "rank" in line, read_file_to_string(run_path + "/last/espreso." + problem + ".log").split("\n")))
+        for field in cells_timers:
+            lines = list(filter(lambda line: field in line, update_lines))
+            if len(lines) >= 2:
+                time = lines[1][70:90].replace(" ", "").replace("-nan", "").replace("nan", "")
+                outstring.write(time)
+            outstring.write(";")
+        outstring.write("\n")
+        runs_finished += 1
+        if runs_finished % 1000 == 0:
+            print("Progress: ", runs_finished, "/", runs_total)
+write_string_to_file(outfile, outstring.getvalue())
+outstring.close()
 
 
 
@@ -154,12 +157,12 @@ runs_finished = 0
 
 outfile = summ_dir + "/apply.csv"
 outstring = io.StringIO()
-cells_info = ["id", "str", "machine", "tool", "problem", "element_type", "domains_x", "domains_y", "domains_z", "n_domains_total", "elements_x", "elements_y", "elements_z", "n_elements_total", "concurrency_set", "concurrency_update", "concurrency_apply", "factor_symmetry_fake", "uniform_clusters_domains", "trsm1_factor_storage", "trsm2_factor_storage", "trsm1_solve_type", "trsm2_solve_type", "trsm_rhs_sol_order", "path_if_hermitian", "f_sharing_if_hermitian", "apply_scatter_gather_where", "transpose_where"]
+cells_info = ["id", "str", "run_id", "machine", "tool", "problem", "element_type", "domains_x", "domains_y", "domains_z", "n_domains_total", "elements_x", "elements_y", "elements_z", "n_elements_total", "concurrency_set", "concurrency_update", "concurrency_apply", "factor_symmetry_fake", "uniform_clusters_domains", "trsm1_factor_storage", "trsm2_factor_storage", "trsm1_solve_type", "trsm2_solve_type", "trsm_rhs_sol_order", "path_if_hermitian", "f_sharing_if_hermitian", "apply_scatter_gather_where", "transpose_where"]
 cells_timers = ["total", "copyin", "scatter", "mv_outer", "mv", "zerofill", "gather", "copyout", "wait"]
 outstring.write(";".join(cells_info))
-outstring.write(";")
+outstring.write(";;")
 outstring.write("error;timeout")
-outstring.write(";")
+outstring.write(";;")
 outstring.write(";".join(cells_timers))
 outstring.write("\n")
 for dir_name in os.listdir(results_dir):
@@ -170,6 +173,8 @@ for dir_name in os.listdir(results_dir):
         for field in cells_info:
             if field == "id" or field == "str" or field == "n_domains_total" or field == "n_elements_total":
                 val = ""
+            elif field == "run_id":
+                val = run_id
             elif field == "problem":
                 problem = list(filter(lambda line: "ecf_file" in line, info_lines))[0].split(".")[1]
                 val = problem
@@ -179,7 +184,7 @@ for dir_name in os.listdir(results_dir):
             outstring.write(";")
 
         outstring.write(";")
-        outstring.write("\"" + read_file_to_string(run_path + "/last/espreso." + problem + ".err").replace("\n", ";").replace("\"", "\"\"") + "\";")
+        outstring.write("\"" + read_file_to_string(run_path + "/last/espreso." + problem + ".err").split("\0")[0].replace("\n", ";").replace("\"", "\"\"") + "\";")
         outstring.write("\"" + read_file_to_string(run_path + "/timeout.txt").replace("\n", ";").replace("\"", "\"\"") + "\";")
         outstring.write(";")
     
@@ -190,7 +195,7 @@ for dir_name in os.listdir(results_dir):
                 times_str = [line[70:90] for line in lines[-10:]]
                 times = [float(tm_str) for tm_str in times_str]
                 avg = sum(times) / len(times)
-                outstring.write(str(avg))
+                outstring.write("{:.6f}".format(avg))
             outstring.write(";")
         outstring.write("\n")
         runs_finished += 1
