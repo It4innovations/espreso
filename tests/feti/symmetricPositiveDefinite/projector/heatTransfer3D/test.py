68185dc1f4b4fde5f7c417ad9e610d4b569627ac
Ondrej Meca <ondrej.meca@vsb.cz>
2024-04-25 14:51:28 +0200

    TEST: HFETI project tests

--- tests/feti/symmetricPositiveDefinite/projector/heatTransfer3D/test.py
+++ tests/feti/symmetricPositiveDefinite/projector/heatTransfer3D/test.py
@@ -6,7 +6,7 @@ class Assembler(unittest.TestCase):
 
     def setUp(self):
         ESPRESOTest.path = os.path.dirname(__file__)
-        ESPRESOTest.args = [ "TETRA4", 2, 2, 1, 2, 2, 2, 4, 4, 8, "ORTHOGONAL" ]
+        ESPRESOTest.args = [ "TETRA4", 2, 2, 1, 2, 2, 2, 4, 4, 8, "TOTAL_FETI", "ORTHOGONAL", 23 ]
         ESPRESOTest.processes = 4
         ESPRESOTest.set_threads(2)
 
@@ -14,10 +14,13 @@ class Assembler(unittest.TestCase):
         ESPRESOTest.clean()
 
     def test_feti(self):
-        for projector in [ "             ORTHOGONAL", "ORTHOGONAL_WITH_FACTORS" ]:
-            yield run, projector
+        for method, max_it in [ (" TOTAL_FETI", 23), ("HYBRID_FETI", 25) ]:
+            for projector in [ "             ORTHOGONAL", "ORTHOGONAL_WITH_FACTORS" ]:
+                yield run, method, projector, max_it
 
-def run(projector):
-    ESPRESOTest.args[10] = projector
+def run(method, projector, max_it):
+    ESPRESOTest.args[10] = method
+    ESPRESOTest.args[11] = projector
+    ESPRESOTest.args[12] = max_it
     ESPRESOTest.run()
     ESPRESOTest.compare_emr("espreso.emr")
--- tests/feti/symmetricPositiveDefinite/projector/linearElasticity2D/test.py
+++ tests/feti/symmetricPositiveDefinite/projector/linearElasticity2D/test.py
@@ -6,7 +6,7 @@ class Assembler(unittest.TestCase):
 
     def setUp(self):
         ESPRESOTest.path = os.path.dirname(__file__)
-        ESPRESOTest.args = [ "TRIANGLE3", 2, 2, 2, 2, 40, 4, "ORTHOGONAL" ]
+        ESPRESOTest.args = [ "TRIANGLE3", 2, 2, 2, 2, 40, 4, "TOTAL_FETI", "ORTHOGONAL", 70 ]
         ESPRESOTest.processes = 4
         ESPRESOTest.set_threads(2)
 
@@ -14,10 +14,13 @@ class Assembler(unittest.TestCase):
         ESPRESOTest.clean()
 
     def test_feti(self):
-        for projector in [ "             ORTHOGONAL", "ORTHOGONAL_WITH_FACTORS" ]:
-                yield run, projector
+        for method, max_it in [ (" TOTAL_FETI", 70), ("HYBRID_FETI", 80) ]:
+            for projector in [ "             ORTHOGONAL", "ORTHOGONAL_WITH_FACTORS" ]:
+                yield run, method, projector, max_it
 
-def run(projector):
-    ESPRESOTest.args[7] = projector
+def run(method, projector, max_it):
+    ESPRESOTest.args[7] = method
+    ESPRESOTest.args[8] = projector
+    ESPRESOTest.args[9] = max_it
     ESPRESOTest.run()
     ESPRESOTest.compare_emr("espreso.emr")
