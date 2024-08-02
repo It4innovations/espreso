650e4ccce99910280988a650b3aee03cd1ba3491
Ondrej Meca <ondrej.meca@vsb.cz>
2024-08-02 12:53:40 +0200

    FIX: preCICE wrapper

--- src/wrappers/precice/w.precice.cpp
+++ src/wrappers/precice/w.precice.cpp
@@ -4,6 +4,7 @@
 #include "basis/containers/serializededata.h"
 #include "esinfo/mpiinfo.h"
 #include "esinfo/meshinfo.h"
+#include "esinfo/eslog.h"
 #include "mesh/store/surfacestore.h"
 
 #include <iostream>
@@ -20,6 +21,9 @@ struct PreciceData {
     }
 
     precice::Participant precice;
+    size_t size;
+    esint *ids;
+    std::vector<double> data;
 };
 }
 
@@ -34,10 +38,14 @@ Precice::Precice(const char *name, bool active)
     if (active) {
         _data = new PreciceData(name);
 
-        size_t size = info::mesh->surface->size;
         double *coords = &info::mesh->surface->coordinates->datatarray().data()->x;
-        esint *ids = info::mesh->surface->nIDs->datatarray().data();
-        _data->precice.setMeshVertices("SolidMesh", precice::span(coords, coords + 3 * size), precice::span(ids, ids + size));
+
+        _data->size = info::mesh->surface->nIDs->datatarray().size();
+        _data->ids = info::mesh->surface->nIDs->datatarray().data();
+        _data->precice.setMeshVertices("SolidMesh", precice::span(coords, coords + 3 * _data->size), precice::span(_data->ids, _data->ids + _data->size));
+        _data->precice.initialize();
+
+        _data->data.resize(3 * _data->size);
     }
 #endif
 }
@@ -55,9 +63,12 @@ void Precice::read(const char *name, double *data, double dt)
 {
 #ifdef HAVE_PRECICE
     if (_data) {
-        size_t size = info::mesh->surface->size;
-        esint *ids = info::mesh->surface->nIDs->datatarray().data();
-        _data->precice.readData("SolidMesh", name, precice::span(ids, ids + size), dt, precice::span(data, data + 3 * size));
+        _data->precice.readData("SolidMesh", name, precice::span(_data->ids, _data->ids + _data->size), dt, _data->data);
+        for (size_t n = 0; n < _data->size; ++n) {
+            for (int d = 0; d < info::mesh->dimension; ++d) {
+                data[_data->ids[n] * info::mesh->dimension + d] = _data->data[n * info::mesh->dimension + d];
+            }
+        }
     }
 #endif
 }
@@ -66,9 +77,12 @@ void Precice::write(const char *name, double *data)
 {
 #ifdef HAVE_PRECICE
     if (_data) {
-        size_t size = info::mesh->surface->size;
-        esint *ids = info::mesh->surface->nIDs->datatarray().data();
-        _data->precice.writeData("SolidMesh", name, precice::span(ids, ids + size), precice::span(data, data + 3 * size));
+        for (size_t n = 0; n < _data->size; ++n) {
+            for (int d = 0; d < info::mesh->dimension; ++d) {
+                _data->data[n * info::mesh->dimension + d] = data[_data->ids[n] * info::mesh->dimension + d];
+            }
+        }
+        _data->precice.writeData("SolidMesh", name, precice::span(_data->ids, _data->ids + _data->size), _data->data);
     }
 #endif
 }
@@ -97,37 +111,59 @@ void Precice::dummy()
 
     Participant participant(solverName, configFileName, info::mpi::rank, info::mpi::size);
 
-    size_t size = info::mesh->surface->size;
+    size_t size = info::mesh->surface->nIDs->datatarray().size();
     double *coords = &info::mesh->surface->coordinates->datatarray().data()->x;
     esint *ids = info::mesh->surface->nIDs->datatarray().data();
-    participant.setMeshVertices("SolidMesh", precice::span(coords, coords + 3 * size), precice::span(ids, ids + size));
+    participant.setMeshVertices("FluidMesh", precice::span(coords, coords + 3 * size), precice::span(ids, ids + size));
 
     std::vector<double> readData(size * 3);
     std::vector<double> writeData(size * 3);
 
+    for (size_t i = 2; i < size * 3; i += 3) {
+        writeData[i] = 1;
+    }
+    if (participant.requiresInitialData()) {
+        participant.writeData(meshName, dataWriteName, precice::span(ids, ids + size), writeData);
+    }
     participant.initialize();
 
     while (participant.isCouplingOngoing()) {
-      if (participant.requiresWritingCheckpoint()) {
-        std::cout << "DUMMY: Writing iteration checkpoint\n";
-      }
+        if (participant.requiresWritingCheckpoint()) {
+            std::cout << "DUMMY: Writing iteration checkpoint\n";
+        }
 
-      double dt = participant.getMaxTimeStepSize();
-      participant.readData(meshName, dataReadName, precice::span(ids, ids + size), dt, readData);
+        double dt = participant.getMaxTimeStepSize();
+        participant.readData(meshName, dataReadName, precice::span(ids, ids + size), dt, readData);
 
-      for (size_t i = 2; i < size * 3; i += 3) {
-        writeData.at(i) = readData.at(i) * 1.5;
-      }
+        for (size_t i = 2; i < size * 3; i += 3) {
+            writeData[i] = readData[i];
+        }
 
-      participant.writeData(meshName, dataWriteName, precice::span(ids, ids + size), writeData);
+        participant.writeData(meshName, dataWriteName, precice::span(ids, ids + size), writeData);
 
-      participant.advance(dt);
+        printf("DISPLACEMENT\n");
+        for (int d = 0; d < 3; ++d) {
+            for (size_t i = d; i < size * 3; i += 3) {
+                printf(" %+e", readData[i]);
+            }
+            printf("\n");
+        }
 
-      if (participant.requiresReadingCheckpoint()) {
-        std::cout << "DUMMY: Reading iteration checkpoint\n";
-      } else {
-        std::cout << "DUMMY: Advancing in time\n";
-      }
+        printf("FORCES\n");
+        for (int d = 0; d < 3; ++d) {
+            for (size_t i = d; i < size * 3; i += 3) {
+                printf(" %+e", writeData[i]);
+            }
+            printf("\n");
+        }
+
+        participant.advance(dt);
+
+        if (participant.requiresReadingCheckpoint()) {
+            std::cout << "DUMMY: Reading iteration checkpoint\n";
+        } else {
+            std::cout << "DUMMY: Advancing in time\n";
+        }
     }
 
     participant.finalize();
