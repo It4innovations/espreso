4ad66aec8454d23e5e1758be5be3f91b3b22f176
Ondrej Meca <ondrej.meca@gmail.com>
2024-10-03 14:23:13 +0200

    TEST: add generated tire test

---
+++ tests/feti/fixedWall/steadystate/linearElasticity3D/tire/test.py
@@ -0,0 +1,18 @@
+
+import os, unittest
+from estest import ESPRESOTest
+
+class Solver(unittest.TestCase):
+
+    def setUp(self):
+        ESPRESOTest.path = os.path.dirname(__file__)
+        ESPRESOTest.args = [ "HEXA8", 2, 2, 1, 2, 2, 4, 4, 4, 16 ]
+        ESPRESOTest.processes = 4
+        ESPRESOTest.set_threads(2)
+
+    def tearDown(self):
+        ESPRESOTest.clean()
+
+    def test_feti(self):
+        ESPRESOTest.run()
+        ESPRESOTest.compare_emr("espreso.emr")
