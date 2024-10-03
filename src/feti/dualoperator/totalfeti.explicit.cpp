7dc485c524d446823f28563ee9a9c8c4062bda93
Ondrej Meca <ondrej.meca@gmail.com>
2024-10-03 08:37:05 +0200

    FIX: explicit dual operator with dynamic B

--- src/feti/dualoperator/totalfeti.explicit.cpp
+++ src/feti/dualoperator/totalfeti.explicit.cpp
@@ -94,6 +94,10 @@ void TotalFETIExplicit<T>::set(const step::Step &step)
 template <typename T>
 void TotalFETIExplicit<T>::update(const step::Step &step)
 {
+    if (feti.updated.B) {
+        d.resize();
+    }
+
     if (feti.updated.K) {
         #pragma omp parallel for
         for (size_t di = 0; di < feti.K.size(); ++di) {
