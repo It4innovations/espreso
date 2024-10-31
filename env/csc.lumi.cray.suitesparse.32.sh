964a3497f14332a35bd6eb9ded2d9ff1cca0b9f7
Ondrej Meca <ondrej.meca@vsb.cz>
2024-10-31 15:58:28 +0200

    ENH: compilation on LUMI

---
+++ env/csc.lumi.cray.suitesparse.32.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ml LUMI/24.03
+ml buildtools/24.03
+ml Boost/1.83.0-cpeCray-24.03
+ml Eigen/3.4.0
+ml buildtools-python/24.03-cray-python3.11
+
+export CXX=CC
+
+. dependencies/install.gklib.sh cc
+. dependencies/install.metis32.sh cc
+. dependencies/install.parmetis32.sh mpicc
+. dependencies/install.suitesparse.sh cc "-DLAPACK_LIBRARIES=/opt/cray/pe/libsci/24.03.0/CRAYCLANG/17.0/x86_64/lib/libsci_cray.so -DBLAS_LIBRARIES=/opt/cray/pe/libsci/24.03.0/CRAYCLANG/17.0/x86_64/lib/libsci_cray.so"
+. dependencies/install.precice.sh mpic++
