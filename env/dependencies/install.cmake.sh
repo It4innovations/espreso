2da5751e48539625935e7406676cc5570a7bb6b6
Jakub Homola <jakub.homola@vsb.cz>
2025-01-08 09:46:08 +0100

    FIX: cmake download

--- env/dependencies/install.cmake.sh
+++ env/dependencies/install.cmake.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]
+if [ $# -lt 2 ]
 then
-  echo "architecture not specified"
+  echo "not enough arguments"
   return 1
 fi
-arch="${1}"
+prefix="${1}"
+arch="${2}"
 
 DEPENDENCIES_DIR="${PWD}/dependencies"
 mkdir -p "${DEPENDENCIES_DIR}"
@@ -17,7 +18,7 @@ CMAKE_DIR="${version_name}"
 CMAKE_ROOT="${DEPENDENCIES_DIR}/${CMAKE_DIR}"
 if [ ! -d "${CMAKE_ROOT}" ]
 then
-  sh ${DEPENDENCIES_DIR}/clone.cmake.sh "${arch}"
+  sh env/dependencies/clone.cmake.sh "${arch}"
 fi
 
 export PATH="${CMAKE_ROOT}/bin:${PATH}"
