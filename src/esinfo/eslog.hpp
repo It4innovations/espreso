faa7b2fc0d564f9a2b69727e10ddaf7a00ef0764
Jakub Homola <jakub.homola@vsb.cz>
2025-03-07 15:13:14 +0100

    ENH: guards for buffer

--- src/esinfo/eslog.hpp
+++ src/esinfo/eslog.hpp
@@ -14,91 +14,121 @@ extern char buffer[BUFFER_SIZE];
 template <typename... Args>
 void info(const char* format, Args... args)
 {
-	if (BUFFER_SIZE < snprintf(buffer, BUFFER_SIZE, format, args...)) {
-		buffer[BUFFER_SIZE - 2] = '\n';
+	#pragma omp critical(eslogbuffer)
+	{
+		if (BUFFER_SIZE < snprintf(buffer, BUFFER_SIZE, format, args...)) {
+			buffer[BUFFER_SIZE - 2] = '\n';
+		}
+		info(buffer);
 	}
-	info(buffer);
 }
 
 template <typename... Args>
 void solver(const char* format, Args... args)
 {
-	if (BUFFER_SIZE < snprintf(buffer, BUFFER_SIZE, format, args...)) {
-		buffer[BUFFER_SIZE - 2] = '\n';
+	#pragma omp critical(eslogbuffer)
+	{
+		if (BUFFER_SIZE < snprintf(buffer, BUFFER_SIZE, format, args...)) {
+			buffer[BUFFER_SIZE - 2] = '\n';
+		}
+		solver(buffer);
 	}
-	solver(buffer);
 }
 
 template <typename... Args>
 void linearsolver(const char* format, Args... args)
 {
-	if (BUFFER_SIZE < snprintf(buffer, BUFFER_SIZE, format, args...)) {
-		buffer[BUFFER_SIZE - 2] = '\n';
+	#pragma omp critical(eslogbuffer)
+	{
+		if (BUFFER_SIZE < snprintf(buffer, BUFFER_SIZE, format, args...)) {
+			buffer[BUFFER_SIZE - 2] = '\n';
+		}
+		linearsolver(buffer);
 	}
-	linearsolver(buffer);
 }
 
 template <typename... Args>
 void duration(const char* format, Args... args)
 {
-	if (BUFFER_SIZE < snprintf(buffer, BUFFER_SIZE, format, args...)) {
-		buffer[BUFFER_SIZE - 2] = '\n';
+	#pragma omp critical(eslogbuffer)
+	{
+		if (BUFFER_SIZE < snprintf(buffer, BUFFER_SIZE, format, args...)) {
+			buffer[BUFFER_SIZE - 2] = '\n';
+		}
+		duration(buffer);
 	}
-	duration(buffer);
 }
 
 template <typename... Args>
 void warning(const char* format, Args... args)
 {
-	if (BUFFER_SIZE < snprintf(buffer, BUFFER_SIZE, format, args...)) {
-		buffer[BUFFER_SIZE - 2] = '\n';
+	#pragma omp critical(eslogbuffer)
+	{
+		if (BUFFER_SIZE < snprintf(buffer, BUFFER_SIZE, format, args...)) {
+			buffer[BUFFER_SIZE - 2] = '\n';
+		}
+		warning(buffer);
 	}
-	warning(buffer);
 }
 
 template <typename... Args>
 void storedata(const char* format, Args... args)
 {
-	if (BUFFER_SIZE < snprintf(buffer, BUFFER_SIZE, format, args...)) {
-		buffer[BUFFER_SIZE - 2] = '\n';
+	#pragma omp critical(eslogbuffer)
+	{
+		if (BUFFER_SIZE < snprintf(buffer, BUFFER_SIZE, format, args...)) {
+			buffer[BUFFER_SIZE - 2] = '\n';
+		}
+		storedata(buffer);
 	}
-	storedata(buffer);
 }
 
 template <typename... Args>
 [[noreturn]] void failure(const char* format, Args... args)
 {
-	if (BUFFER_SIZE < snprintf(buffer, BUFFER_SIZE, format, args...)) {
-		buffer[BUFFER_SIZE - 2] = '\n';
+	#pragma omp critical(eslogbuffer)
+	{
+		if (BUFFER_SIZE < snprintf(buffer, BUFFER_SIZE, format, args...)) {
+			buffer[BUFFER_SIZE - 2] = '\n';
+		}
+		failure(buffer);
 	}
-	failure(buffer);
 }
 
 template <typename... Args>
 [[noreturn]] void internalFailure(const char* format, Args... args)
 {
-	if (BUFFER_SIZE < snprintf(buffer, BUFFER_SIZE, format, args...)) {
-		buffer[BUFFER_SIZE - 2] = '\n';
+	#pragma omp critical(eslogbuffer)
+	{
+		if (BUFFER_SIZE < snprintf(buffer, BUFFER_SIZE, format, args...)) {
+			buffer[BUFFER_SIZE - 2] = '\n';
+		}
+		internalFailure(buffer);
 	}
-	internalFailure(buffer);
 }
 
 template <typename... Args>
 [[noreturn]] void error(const char* format, Args... args)
 {
-	if (BUFFER_SIZE < snprintf(buffer, BUFFER_SIZE, format, args...)) {
-		buffer[BUFFER_SIZE - 2] = '\n';
+	#pragma omp critical(eslogbuffer)
+	{
+		if (BUFFER_SIZE < snprintf(buffer, BUFFER_SIZE, format, args...)) {
+			buffer[BUFFER_SIZE - 2] = '\n';
+		}
+		error(buffer);
 	}
-	error(buffer);
 }
 
 template <typename... Args>
 [[noreturn]] void globalerror(const char* format, Args... args)
 {
-	if (BUFFER_SIZE < snprintf(buffer, BUFFER_SIZE, format, args...)) {
-		buffer[BUFFER_SIZE - 2] = '\n';
+	#pragma omp critical(eslogbuffer)
+	{
+		if (BUFFER_SIZE < snprintf(buffer, BUFFER_SIZE, format, args...)) {
+			buffer[BUFFER_SIZE - 2] = '\n';
+		}
+		globalerror(buffer);
 	}
-	globalerror(buffer);
 }
 
 }
